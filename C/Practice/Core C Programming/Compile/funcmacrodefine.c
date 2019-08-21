31c3a4535497228a3320006adce5397753f485e5
HyeongminKim <assa0620@gmail.com>
2019-08-21 20:47:44 +0900

    new file:   funcmacrodefine.c

---
+++ C/Practice/Core C Programming/Compile/funcmacrodefine.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define SQUARE(x) ((x) * (x))
+
+int square(int x) {
+    return x * x;
+}
+
+int main() {
+    int a = 5;
+    printf("SQUARE(10) = %d\n", SQUARE(10));
+    printf("SQUARE(1.5) = %f\n", SQUARE(1.5));
+    printf("SQUARE(\'a\') = %d\n", SQUARE('a'));
+//  printf("SQUARE(\"a\") = %d\n", SQUARE("a"));
+
+    printf("square(1 + 2) = %d\n", square(1 + 2));
+    printf("SQUARE(1 + 2) = %d\n", SQUARE(1 + 2));
+
+    printf("square(++a) = %d\n", square(++a));
+    a = 5;
+    printf("SQUARE(++a) = %d\n", SQUARE(++a));
+
+    printf("square(3.1) = %d\n", square(3.1));
+    printf("SQUARE(3.1) = %d\n", SQUARE(3.1));
+
+    return 0;
+}
