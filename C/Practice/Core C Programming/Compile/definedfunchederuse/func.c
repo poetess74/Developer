25a54ee9b62ec88920470de3a1e4dcfc43bc7445
HyeongminKim <assa0620@gmail.com>
2019-08-26 16:53:19 +0900

    new file:   func.c 	new file:   func.h 	new file:   main.c 	Untracked:  ../ifsortselect.c 	Untracked:  ../internalmacrouse.c 	Untracked:  ../multipleifdefuse.c

---
+++ C/Practice/Core C Programming/Compile/definedfunchederuse/func.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "func.h"
+
+int get_factorial(int num) {
+    int result = 1;
+    for (int i = 1; i <= num; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+int get_gcd(int x, int y) {
+    int r;
+    while (y != 0) {
+        r = x % y;
+        x = y;
+        y = r;
+    }
+    return x;
+}
+
+int get_max(int a, int b, int c) {
+    int max = a > b ? a : b;
+    max = c > max ? c : max;
+    return max;
+}
+
+void print_array(const int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%2d", array[i]);
+    }
+    printf("\n");
+}
