a599449da68b3067f8bcfec3ef011db80e5712cd
HyeongminKim <assa0620@gmail.com>
2019-08-26 21:36:19 +0900

    new file:   multipleheaderdefense/line.h 	new file:   multipleheaderdefense/main.c 	new file:   multipleheaderdefense/point.h

---
+++ C/Practice/Core C Programming/Compile/multipleheaderdefense/point.h
@@ -0,0 +1,21 @@
+#ifndef POINT_H
+#define POINT_H
+
+typedef struct point {
+    int x, y;
+} POINT;
+
+void print_point(const POINT *pt);
+void set_point(POINT *pt, int x, int y);
+int is_equal_point(const POINT *lhs, const POINT *rhs);
+double get_length(const POINT *start, const POINT *end);
+
+#define PRT_POINT(pt) {\
+    printf(#pt " = ");\
+    print_point(&pt);\
+    printf("\n");\
+}
+
+extern POINT origin;
+
+#endif
