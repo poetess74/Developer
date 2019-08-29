79bc495be8ced9990c513b1740d794a8ec2d23a4
HyeongminKim <assa0620@gmail.com>
2019-08-29 16:27:01 +0900

    modified:   17004s1.c

--- C/Practice/자기주도 C언어 프로그래밍/Chapter17/17004s1.c
+++ C/Practice/자기주도 C언어 프로그래밍/Chapter17/17004s1.c
@@ -4,7 +4,16 @@ int main() {
     int a[5];
     int *p = a;
 
-    scanf("%d %d %d %d %d", &*p, &*(p + 1), &*(p + 2), &*(p + 3), &*(p + 4));
-    printf("%d %d %d\n", *p, *(p + 2), *(p + 4));
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &*(p + i);
+    }
+
+//  scanf("%d %d %d %d %d", &*p, &*(p + 1), &*(p + 2), &*(p + 3), &*(p + 4));
+
+    for (int i = 0; i < 5; i += 2) {
+        printf("%d ", *(p + i));
+    }
+
+//  printf("%d %d %d\n", *p, *(p + 2), *(p + 4));
     return 0;
 }
