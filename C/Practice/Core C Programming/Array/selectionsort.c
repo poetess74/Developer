73d2baedd5fffbd670213fed038b947d7daece72
HyeongminKim <assa0620@gmail.com>
2019-08-29 18:55:23 +0900

    modified:   C/Practice/Core C Programming/Array/selectionsort.c 	renamed:    C/Practice/Core C Programming/test4s1.c -> C/Practice/Core C Programming/Variable/fronthandandbackhand.c 	modified:   "C/Practice/\354\236\220\352\270\260\354\243\274\353\217\204 C\354\226\270\354\226\264 \355\224\204\353\241\234\352\267\270\353\236\230\353\260\215/Chapter17/17004s1.c"

--- C/Practice/Core C Programming/Array/selectionsort.c
+++ C/Practice/Core C Programming/Array/selectionsort.c
@@ -4,17 +4,41 @@
 int main() {
     int data[SIZE] = {7, 3, 9, 5, 1};
     int index, temp;
-    for (int i = 0; i < SIZE; i++) {
-        index = i;
-        for (int j = i + 1; j < SIZE; j++) {
-            if (data[index] > data[j]) {
-                index = j;
+    while (1) {
+        printf("input : ");
+        scanf("%d", &temp);
+        if (temp == 0 || temp == 1) {
+            break;
+        }
+        printf("try again\n");
+    }
+    if (temp) {
+        for (int i = 0; i < SIZE; i++) {
+            index = i;
+            for (int j = i + 1; j < SIZE; j++) {
+                if (data[index] > data[j]) {
+                    index = j;
+                }
+            }
+            if (i != index) {
+                temp = data[i];
+                data[i] = data[index];
+                data[index] = temp;
             }
         }
-        if (i != index) {
-            temp = data[i];
-            data[i] = data[index];
-            data[index] = temp;
+    } else {
+         for (int i = 0; i < SIZE; i++) {
+            index = i;
+            for (int j = i + 1; j > SIZE; j++) {
+                if (data[index] > data[j]) {
+                    index = j;
+                }
+            }
+            if (i != index) {
+                temp = data[i];
+                data[i] = data[index];
+                data[index] = temp;
+            }
         }
     }
     printf("result : ");
--- C/Practice/자기주도 C언어 프로그래밍/Chapter17/17004s1.c
+++ C/Practice/자기주도 C언어 프로그래밍/Chapter17/17004s1.c
@@ -5,15 +5,12 @@ int main() {
     int *p = a;
 
     for (int i = 0; i < 5; i++) {
-        scanf("%d", &*(p + i);
+        scanf("%d", &*(p + i));
     }
-
-//  scanf("%d %d %d %d %d", &*p, &*(p + 1), &*(p + 2), &*(p + 3), &*(p + 4));
-
     for (int i = 0; i < 5; i += 2) {
         printf("%d ", *(p + i));
     }
+    printf("\n");
 
-//  printf("%d %d %d\n", *p, *(p + 2), *(p + 4));
     return 0;
 }
