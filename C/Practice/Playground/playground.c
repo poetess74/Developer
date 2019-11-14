059305e645cda83b2c14230a6667db5cdb0ec07c
HyeongminKim <assa0620@gmail.com>
2019-11-14 13:59:25 +0900

    new file:   playground.c 	renamed:    main.c -> study.c

---
+++ C/Practice/Playground/playground.c
@@ -0,0 +1,34 @@
+//
+//  main.c
+//  C programing
+//
+//  Created by HyeongminKim on 2014. 2. 21.
+//  Copyright (c) 2014년 HyeongminKim. All rights reserved.
+//
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int a = 10;
+    int b = 20;
+    int *p1 = &a;
+    const int *constp = (int*)0xffffff; // macOs default var init value is 0 (0x0)
+    int * const intconstp = &a;
+    const int * const constintconst = &a;
+// --------------------------------------------------
+//  printf("input : ");
+//  scanf("%d", &a);
+    printf("a = %d\n", a);
+    printf("p = %p\n", p1);
+    printf("&p = %p\n", &p1);
+    printf("constp = %p\n", constp);
+    printf("intconstp = %p\n", intconstp);
+    printf("constintconst = %p\n", constintconst);
+    *p1 = b;
+//  *constp = b;          // read-only variable is not assignable
+//  intconstp = &b;       // cannot assign to variable 'intconstp' with const-qualified type 'int *const'
+//  *constintconst = b;   // read-only variable is not assignable
+//  ---------------------------------------------------    
+    return 0;
+}
--- C/Practice/Playground/study.c
+++ C/Practice/Playground/study.c
@@ -10,26 +10,6 @@
 #include <string.h>
 
 int main(){
-    int a = 10;
-    int b = 20;
-    int *p1 = &a;
-    const int *constp = (int*)0xffffff; // macOs default var init value is 0 (0x0)
-    int * const intconstp = &a;
-    const int * const constintconst = &a;
-// --------------------------------------------------
-//  printf("input : ");
-//  scanf("%d", &a);
-    printf("a = %d\n", a);
-    printf("p = %p\n", p1);
-    printf("&p = %p\n", &p1);
-    printf("constp = %p\n", constp);
-    printf("intconstp = %p\n", intconstp);
-    printf("constintconst = %p\n", constintconst);
-    *p1 = b;
-//  *constp = b;          // read-only variable is not assignable
-//  intconstp = &b;       // cannot assign to variable 'intconstp' with const-qualified type 'int *const'
-//  *constintconst = b;   // read-only variable is not assignable
-//  ---------------------------------------------------    
     char s1[10];
     char s2[10];
     char s3[3];
