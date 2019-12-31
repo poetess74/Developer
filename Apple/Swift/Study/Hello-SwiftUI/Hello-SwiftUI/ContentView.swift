2d68f1d8d924964c49692992e7dcd92db9dfa50e
HyeongminKim <assa0620@gmail.com>
2019-12-31 21:19:42 +0900

    renamed:    Apple/Swift/170808.playground/Contents.swift -> Apple/Swift/Practice/170808.playground/Contents.swift 	renamed:    Apple/Swift/170808.playground/contents.xcplayground -> Apple/Swift/Practice/170808.playground/contents.xcplayground 	renamed:    Apple/Swift/170808.playground/playground.xcworkspace/contents.xcworkspacedata -> Apple/Swift/Practice/170808.playground/playground.xcworkspace/contents.xcworkspacedata 	renamed:    Apple/Swift/170808.playground/playground.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate -> Apple/Swift/Practice/170808.playground/playground.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	renamed:    Apple/Swift/181212.playground/Contents.swift -> Apple/Swift/Practice/181212.playground/Contents.swift 	renamed:    Apple/Swift/181212.playground/contents.xcplayground -> Apple/Swift/Practice/181212.playground/contents.xcplayground 	renamed:    Apple/Swift/181212.playground/playground.xcworkspace/contents.xcworkspacedata -> Apple/Swift/Practice/181212.playground/playground.xcworkspace/contents.xcworkspacedata 	renamed:    Apple/Swift/181212.playground/playground.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist -> Apple/Swift/Practice/181212.playground/playground.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist 	renamed:    Apple/Swift/181212.playground/playground.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate -> Apple/Swift/Practice/181212.playground/playground.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI.xcodeproj/project.pbxproj 	new file:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	new file:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI.xcodeproj/project.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist 	new file:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI.xcodeproj/project.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI.xcodeproj/xcuserdata/hm.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI/AppDelegate.swift 	new file:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI/Assets.xcassets/AppIcon.appiconset/Contents.json 	new file:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI/Assets.xcassets/Contents.json 	new file:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI/Base.lproj/LaunchScreen.storyboard 	new file:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI/ContentView.swift 	new file:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI/Info.plist 	new file:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI/Preview Content/Preview Assets.xcassets/Contents.json 	new file:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI/SceneDelegate.swift 	new file:   Apple/Swift/Study/SwiftUI-demo.playground/Contents.swift 	new file:   Apple/Swift/Study/SwiftUI-demo.playground/contents.xcplayground

---
+++ Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello-SwiftUI
+//
+//  Created by HM on 12/31/19.
+//  Copyright © 2019 HM. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello SwiftUI!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
