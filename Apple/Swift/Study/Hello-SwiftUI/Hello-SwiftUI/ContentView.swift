91e803442f6863f386d8bb8236ee90b09d9cb8ba
HyeongminKim <assa0620@gmail.com>
2019-12-31 21:44:36 +0900

    modified:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI.xcodeproj/project.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	modified:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI/ContentView.swift

--- Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI/ContentView.swift
+++ Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI/ContentView.swift
@@ -10,7 +10,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello SwiftUI!")
+        VStack(alignment: .center) {
+            Text("First Line")
+                .font(.largeTitle)
+                .foregroundColor(.green)
+            
+            Text("Second Line")
+                .font(.title)
+                .foregroundColor(.orange)
+            
+            HStack {
+                Text("Left Side")
+                Text("Right Side")
+                    .padding(.all)
+            }
+        }
     }
 }
 
