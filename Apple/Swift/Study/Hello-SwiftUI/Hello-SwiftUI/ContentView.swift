fb84ef2b2d1bd51638f9fc415cf8db6f585a6b70
HyeongminKim <assa0620@gmail.com>
2020-01-01 22:47:10 +0900

    modified:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI.xcodeproj/project.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI/Assets.xcassets/costa-rica.imageset/Contents.json 	new file:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI/Assets.xcassets/costa-rica.imageset/Screen Shot 2019-06-06 at 6.59.42 PM.png 	modified:   Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI/ContentView.swift

--- Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI/ContentView.swift
+++ Apple/Swift/Study/Hello-SwiftUI/Hello-SwiftUI/ContentView.swift
@@ -11,6 +11,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(alignment: .center) {
+            Image("costa-rica")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(50)
+                .padding(.all)
+//                .clipShape(Circle())
             Text("First Line")
                 .font(.largeTitle)
                 .foregroundColor(.green)
