c5e23197b2c733fdb2b054220948fcfa5e6eee0c
HyeongminKim <assa0620@gmail.com>
2020-01-03 18:06:55 +0900

    modified:   Apple/Swift/Study/Hiking/Hiking.xcodeproj/project.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	modified:   Apple/Swift/Study/Hiking/Hiking/ContentView.swift 	new file:   Apple/Swift/Study/Hiking/Hiking/Preview Content/Preview Assets.xcassets/sal.imageset/Contents.json 	new file:   Apple/Swift/Study/Hiking/Hiking/Preview Content/Preview Assets.xcassets/sal.imageset/Screen Shot 2019-06-06 at 7.52.51 PM.png 	new file:   Apple/Swift/Study/Hiking/Hiking/Preview Content/Preview Assets.xcassets/tam.imageset/Contents.json 	new file:   Apple/Swift/Study/Hiking/Hiking/Preview Content/Preview Assets.xcassets/tam.imageset/Screen Shot 2019-06-06 at 7.53.10 PM.png 	new file:   Apple/Swift/Study/Hiking/Hiking/Preview Content/Preview Assets.xcassets/tom.imageset/Contents.json 	new file:   Apple/Swift/Study/Hiking/Hiking/Preview Content/Preview Assets.xcassets/tom.imageset/Screen Shot 2019-06-06 at 7.53.02 PM.png

--- Apple/Swift/Study/Hiking/Hiking/ContentView.swift
+++ Apple/Swift/Study/Hiking/Hiking/ContentView.swift
@@ -11,8 +11,8 @@ import SwiftUI
 struct ContentView: View {
     let hikes = Hike.all()
     var body: some View {
-        List(self.hikes, id: \.name) {
-            hike in Text(hike.name)
+        List(self.hikes, id: \.name) { hike in
+            HikeCell(hike: hike)
         }
     }
 }
@@ -20,5 +20,21 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct HikeCell: View {
+    let hike: Hike
+    var body: some View {
+        HStack {
+            Image(hike.imageURL)
+                .resizable()
+                .frame(width: 100, height: 100)
+                .cornerRadius(16)
+            VStack(alignment: .leading) {
+                Text(hike.name)
+                Text(String(format: "%.2f", hike.miles))
+            }
+        }
     }
 }
