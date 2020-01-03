2cc6f4586296dbe36c2f8537ca7ed21f31421223
HyeongminKim <assa0620@gmail.com>
2020-01-03 18:21:16 +0900

    modified:   Apple/Swift/Study/Hiking/Hiking.xcodeproj/project.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	modified:   Apple/Swift/Study/Hiking/Hiking/ContentView.swift

--- Apple/Swift/Study/Hiking/Hiking/ContentView.swift
+++ Apple/Swift/Study/Hiking/Hiking/ContentView.swift
@@ -11,8 +11,13 @@ import SwiftUI
 struct ContentView: View {
     let hikes = Hike.all()
     var body: some View {
-        List(self.hikes, id: \.name) { hike in
-            HikeCell(hike: hike)
+        NavigationView {
+            List(self.hikes, id: \.name) {
+                hike in NavigationLink(destination: Text(hike.name)) {
+                    HikeCell(hike: hike)
+                }
+            }
+            .navigationBarTitle("Hikings")
         }
     }
 }
