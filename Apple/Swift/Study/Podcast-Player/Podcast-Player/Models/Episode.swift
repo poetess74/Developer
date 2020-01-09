17d1aba1013d82d92b49b129411cf69270d2264d
HyeongminKim <assa0620@gmail.com>
2020-01-09 14:37:17 +0900

    new file:   Apple/Swift/Study/Podcast-Player/Podcast-Player.xcodeproj/project.pbxproj 	new file:   Apple/Swift/Study/Podcast-Player/Podcast-Player.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	new file:   Apple/Swift/Study/Podcast-Player/Podcast-Player.xcodeproj/project.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist 	new file:   Apple/Swift/Study/Podcast-Player/Podcast-Player.xcodeproj/project.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   Apple/Swift/Study/Podcast-Player/Podcast-Player.xcodeproj/xcuserdata/hm.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   Apple/Swift/Study/Podcast-Player/Podcast-Player/AppDelegate.swift 	new file:   Apple/Swift/Study/Podcast-Player/Podcast-Player/Assets.xcassets/AppIcon.appiconset/Contents.json 	new file:   Apple/Swift/Study/Podcast-Player/Podcast-Player/Assets.xcassets/Contents.json 	new file:   Apple/Swift/Study/Podcast-Player/Podcast-Player/Base.lproj/LaunchScreen.storyboard 	new file:   Apple/Swift/Study/Podcast-Player/Podcast-Player/ContentView.swift 	new file:   Apple/Swift/Study/Podcast-Player/Podcast-Player/Info.plist 	new file:   Apple/Swift/Study/Podcast-Player/Podcast-Player/Models/Episode.swift 	new file:   Apple/Swift/Study/Podcast-Player/Podcast-Player/Preview Content/Preview Assets.xcassets/Contents.json 	new file:   Apple/Swift/Study/Podcast-Player/Podcast-Player/SceneDelegate.swift

---
+++ Apple/Swift/Study/Podcast-Player/Podcast-Player/Models/Episode.swift
@@ -0,0 +1,14 @@
+//
+//  Episode.swift
+//  Podcast-Player
+//
+//  Created by HM on 1/9/20.
+//  Copyright © 2020 HM. All rights reserved.
+//
+
+import Foundation
+
+struct Episode {
+    let name: String
+    let track: String
+}
