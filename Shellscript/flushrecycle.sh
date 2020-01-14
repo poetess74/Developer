b23222ced4c0ee637900499f75964e1f18f07b21
HyeongminKim <assa0620@gmail.com>
2020-01-14 19:03:51 +0900

    modified:   flushrecycle.sh

--- Shellscript/flushrecycle.sh
+++ Shellscript/flushrecycle.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "휴지통에 있는 항목들을 영구적으로 지우겠습니까?"
-echo -n "이 동작은 실행 취소할 수 없습니다. (y/N) "
+ls ~/.Trash
+echo -n "이 동작은 실행 취소할 수 없습니다. (y/N) > "
 read input
 if [ "$input" == "y" -o "$input" == "Y" ]; then
     rm -rf ~/.Trash/*
