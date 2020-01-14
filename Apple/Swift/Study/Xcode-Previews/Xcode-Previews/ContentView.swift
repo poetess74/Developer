62764d60bf7d27da9e9cf8e89f54a48ee0a22985
HyeongminKim <assa0620@gmail.com>
2020-01-14 15:46:26 +0900

    new file:   Apple/Swift/Study/Xcode-Previews/Xcode-Previews.xcodeproj/project.pbxproj 	new file:   Apple/Swift/Study/Xcode-Previews/Xcode-Previews.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	new file:   Apple/Swift/Study/Xcode-Previews/Xcode-Previews.xcodeproj/project.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist 	new file:   Apple/Swift/Study/Xcode-Previews/Xcode-Previews.xcodeproj/project.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   Apple/Swift/Study/Xcode-Previews/Xcode-Previews.xcodeproj/xcuserdata/hm.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   Apple/Swift/Study/Xcode-Previews/Xcode-Previews/AppDelegate.swift 	new file:   Apple/Swift/Study/Xcode-Previews/Xcode-Previews/Assets.xcassets/AppIcon.appiconset/Contents.json 	new file:   Apple/Swift/Study/Xcode-Previews/Xcode-Previews/Assets.xcassets/Contents.json 	new file:   Apple/Swift/Study/Xcode-Previews/Xcode-Previews/Base.lproj/LaunchScreen.storyboard 	new file:   Apple/Swift/Study/Xcode-Previews/Xcode-Previews/ContentView.swift 	new file:   Apple/Swift/Study/Xcode-Previews/Xcode-Previews/Info.plist 	new file:   Apple/Swift/Study/Xcode-Previews/Xcode-Previews/Preview Content/Preview Assets.xcassets/Contents.json 	new file:   Apple/Swift/Study/Xcode-Previews/Xcode-Previews/SceneDelegate.swift

---
+++ Apple/Swift/Study/Xcode-Previews/Xcode-Previews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Xcode-Previews
+//
+//  Created by HM on 1/14/20.
+//  Copyright © 2020 HM. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World 2")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
