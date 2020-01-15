9c3a4f675caecbde4f5b58a2626a1dc1840e8f1c
HyeongminKim <assa0620@gmail.com>
2020-01-15 15:32:55 +0900

    modified:   Apple/Swift/Study/BarGraphDemo/BarGraphDemo.xcodeproj/project.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	modified:   Apple/Swift/Study/BarGraphDemo/BarGraphDemo/BarGraph.swift 	modified:   Apple/Swift/Study/BarGraphDemo/BarGraphDemo/Report.swift

--- Apple/Swift/Study/BarGraphDemo/BarGraphDemo/Report.swift
+++ Apple/Swift/Study/BarGraphDemo/BarGraphDemo/Report.swift
@@ -16,9 +16,9 @@ struct Report: Hashable {
 extension Report {
     static func all() -> [Report] {
         return [
-            Report(year: "2001", revenue: 2500),
-            Report(year: "2002", revenue: 4500),
-            Report(year: "2003", revenue: 6500)
+            Report(year: "2001", revenue: 1500),
+            Report(year: "2002", revenue: 3500),
+            Report(year: "2003", revenue: 8500)
         ]
     }
 }
