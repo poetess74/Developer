0105856243f1086c9f0167ef46417ffea4da62dc
HyeongminKim <assa0620@gmail.com>
2020-01-15 16:47:34 +0900

    modified:   Apple/Swift/Study/gestures-demo/gestures-demo.xcodeproj/project.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	modified:   Apple/Swift/Study/gestures-demo/gestures-demo/ContentView.swift

--- Apple/Swift/Study/gestures-demo/gestures-demo/ContentView.swift
+++ Apple/Swift/Study/gestures-demo/gestures-demo/ContentView.swift
@@ -10,8 +10,19 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var tapped: Bool = false
+    @State private var cardDragState =  CGSize.zero
     var body: some View {
         Card(tapped: self.tapped)
+            .animation(.spring())
+            .offset(y: self.cardDragState.height)
+            .gesture(DragGesture()
+                .onChanged { value in
+                    self.cardDragState = value.translation
+                }
+                .onEnded { value in
+                    self.cardDragState = CGSize.zero
+                }
+            )
             .gesture(TapGesture(count: 1)
                 .onEnded({ () in
                     self.tapped.toggle()
