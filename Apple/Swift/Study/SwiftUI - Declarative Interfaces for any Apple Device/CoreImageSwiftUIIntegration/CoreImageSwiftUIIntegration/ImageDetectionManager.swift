91ea90b4e7db7178283eebc96a1e2070ff732d30
HyeongminKim <assa0620@gmail.com>
2020-01-17 17:47:34 +0900

    modified:   Apple/Swift/Study/SwiftUI - Declarative Interfaces for any Apple Device/CoreImageSwiftUIIntegration/CoreImageSwiftUIIntegration.xcodeproj/project.pbxproj 	modified:   Apple/Swift/Study/SwiftUI - Declarative Interfaces for any Apple Device/CoreImageSwiftUIIntegration/CoreImageSwiftUIIntegration.xcodeproj/project.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	modified:   Apple/Swift/Study/SwiftUI - Declarative Interfaces for any Apple Device/CoreImageSwiftUIIntegration/CoreImageSwiftUIIntegration/ImageDetectionManager.swift 	new file:   Apple/Swift/Study/SwiftUI - Declarative Interfaces for any Apple Device/CoreImageSwiftUIIntegration/CoreImageSwiftUIIntegration/Resnet50.mlmodel 	new file:   Apple/Swift/Study/SwiftUI - Declarative Interfaces for any Apple Device/CoreImageSwiftUIIntegration/CoreImageSwiftUIIntegration/UIImage+Extensions.swift

--- Apple/Swift/Study/SwiftUI - Declarative Interfaces for any Apple Device/CoreImageSwiftUIIntegration/CoreImageSwiftUIIntegration/ImageDetectionManager.swift
+++ Apple/Swift/Study/SwiftUI - Declarative Interfaces for any Apple Device/CoreImageSwiftUIIntegration/CoreImageSwiftUIIntegration/ImageDetectionManager.swift
@@ -7,7 +7,16 @@
 //
 
 import Foundation
+import CoreML
+import UIKit
 
 class ImageDetectionManager {
-    
+    let model = Resnet50()
+    func detect(_ img: UIImage) -> String? {
+        guard let pixelBuffer = img.toCVPixelBuffer(),
+            let prediction = try? model.prediction(image: pixelBuffer) else {
+                return nil
+        }
+        return prediction.classLabel
+    }
 }
