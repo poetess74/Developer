ec2071a2ba890023ed02f5bf4877380d07d5d75e
HyeongminKim <assa0620@gmail.com>
2020-01-21 15:14:42 +0900

    modified:   Shellscript/aptall.sh

--- Shellscript/aptall.sh
+++ Shellscript/aptall.sh
@@ -6,6 +6,41 @@ update=false
 upgrade=false
 cleanup=false
 
+function printInit() {
+    echo '#################### INSTALL GUIDE ###################'
+    echo 'TODO: INSERT this command to your $SHELL_CONFIG_FILE (except "#!>", "<!#"). '
+    echo 'NOTE: This command tested on zsh.'
+    echo ''
+    echo '#!> echo 'alias aptall="$CUSTOMPATH/aptall.sh;echo \"\a\""' >> $SHELL_CONFIG_FILE && source $SHELL_CONFIG_FILE; chmod 550 $CUSTOMPATH/aptall.sh; touch ~/.config/greengecko-aptall.initializationed <!#'
+    echo ''
+    echo 'ex) echo 'alias aptall="~/.etc/aptall.sh;upgrade_oh_my_zsh;echo \"\a\""' >> ~/.zshrc && source ~/.zshrc; chmod 550 ~/.etc/aptall.sh; touch ~/.config/greengecko-aptall.initializationed'
+    echo ''
+    echo 'WARN: Do not modify ">>" to ">". You will lost your $SHELL_CONFIG_FILE forever.'
+    echo '      Do not grant write permission all user account. (except path change) '
+    echo '          And you should grant your user account permission read and execute. (Recommend grant mode: 550, 500)'
+    echo ''
+    echo 'INFO: This command will set alias "aptall" in your shell config.'
+    echo '      When "aptall" command entered run this script, upgrade oh_my_zsh. '
+    echo '      And this script will update brew and save logs to ~/.local/logs'
+    echo '######################################################'
+}
+
+ls ~/.config 2>/dev/null | grep greengecko-aptall.initializationed > /dev/null 2>&1
+if [ "$?" != "0" ]; then
+    inited=false
+    echo " WARN: Please add \"init\" option to run faster this script."
+    echo "USAGE: ./aptall.sh init"
+    echo " INFO: This option will print install guide."
+fi
+
+if [ "$1" == "init" ]; then
+    if [ "$inited" == "false" ]; then
+        printInit
+    else
+        echo "You are already configured this script."
+    fi
+fi
+
 echo -e "\033[32mInitiated time: $(date)\033[m"
 
 sudo echo "null" &> /dev/null
