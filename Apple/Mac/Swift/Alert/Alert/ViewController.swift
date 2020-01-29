3251587cab94978914a3ca75bd3c16e0bbc2adcc
HyeongminKim <assa0620@gmail.com>
2020-01-29 15:21:42 +0900

    modified:   Apple/Mac/Swift/Alert/Alert-iOS/Info.plist 	modified:   Apple/Mac/Swift/Alert/Alert.xcodeproj/project.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	modified:   Apple/Mac/Swift/Alert/Alert/ViewController.swift

--- Apple/Mac/Swift/Alert/Alert/ViewController.swift
+++ Apple/Mac/Swift/Alert/Alert/ViewController.swift
@@ -30,26 +30,26 @@ struct ViewController: View {
                         self.StoreDrive.alertColor = .blue
                         self.StoreDrive.altStatus = 1
                         playSound(source: self.StoreDrive.altStatus - 1)
-                    }.foregroundColor(.blue)
+                    }.foregroundColor(.blue).fixedSize().padding()
                     Button("Yellow Alert") {
                         self.StoreDrive.bgColor = .yellow
                         self.StoreDrive.alertColor = .yellow
                         self.StoreDrive.altStatus = 2
                         playSound(source: self.StoreDrive.altStatus - 1)
-                    }.foregroundColor(.yellow)
+                    }.foregroundColor(.yellow).fixedSize().padding()
                     Button("Red Alert") {
                         self.StoreDrive.bgColor = .red
                         self.StoreDrive.alertColor = .red
                         self.StoreDrive.altStatus = 3
                         playSound(source: self.StoreDrive.altStatus - 1)
-                    }.foregroundColor(.red)
+                    }.foregroundColor(.red).fixedSize().padding()
                     Button("Black Alert") {
                         self.StoreDrive.bgColor = .black
                         self.StoreDrive.alertColor = Color.init(red: 0, green: 1, blue: 1, opacity: 1)
                         self.StoreDrive.altStatus = 4
                         playSound(source: self.StoreDrive.altStatus - 1)
-                    }.foregroundColor(.black)
-                }.padding()
+                    }.foregroundColor(Color.init(red: 0, green: 1, blue: 1, opacity: 1)).fixedSize().padding()
+                }.padding().padding()
                 if (StoreDrive.altStatus != 0) {
                     Button("Clear Alert") {
                         self.StoreDrive.bgColor = .white
