5d6520e1cc8c93e17dd7ef0ae08ee05b5cc101c5
HyeongminKim <assa0620@gmail.com>
2020-02-16 20:09:35 +0900

    modified:   Apple/Mac/SwiftUI/Recruitment Assistant/Recruitment Assistant.xcodeproj/project.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	modified:   Apple/Mac/SwiftUI/Recruitment Assistant/Recruitment Assistant/AppDelegate.swift

--- Apple/Mac/SwiftUI/Recruitment Assistant/Recruitment Assistant/AppDelegate.swift
+++ Apple/Mac/SwiftUI/Recruitment Assistant/Recruitment Assistant/AppDelegate.swift
@@ -35,6 +35,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
-
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        exit(EXIT_SUCCESS)
+    }
 }
 
