9118086ebf0039d0123178bf299c360812f19ea4
HyeongminKim <assa0620@gmail.com>
2020-02-16 22:02:16 +0900

    modified:   Apple/Mac/SwiftUI/Recruitment Assistant/Recruitment Assistant.xcodeproj/project.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	modified:   Apple/Mac/SwiftUI/Recruitment Assistant/Recruitment Assistant/RAChange.swift

--- Apple/Mac/SwiftUI/Recruitment Assistant/Recruitment Assistant/RAChange.swift
+++ Apple/Mac/SwiftUI/Recruitment Assistant/Recruitment Assistant/RAChange.swift
@@ -10,8 +10,11 @@ import SwiftUI
 
 struct RAChange: View {
     @EnvironmentObject var UserDB: UserDB
+    @State private var captchaInput = ""
     @State private var adminID = ""
     @State private var adminPW = ""
+    @State private var title = ""
+    @State private var message = ""
     @State private var isAlert = false
     @State private var captcha = arc4random_uniform(8999) + 1000
     var body: some View {
@@ -29,32 +32,50 @@ struct RAChange: View {
                         Text("만약 현행 유지하고 싶으면 비워두기")
                         TextField("변경할 관계자 아이디", text: self.$adminID).frame(width: 150)
                         SecureField("변경할 관계자 비밀번호", text: self.$adminPW).frame(width: 150)
+                        SecureField("변경할 비밀번호 검증", text: self.$captchaInput).frame(width: 150)
                     } else {
                         Text("\(self.captcha)")
                             .font(.title).padding().fixedSize()
-                        TextField("숫자만 입력", text: self.$adminID).frame(width: 150)
+                        TextField("캡챠 숫자만 입력", text: self.$captchaInput).frame(width: 150)
+                        TextField("기본 관계자 아이디 입력", text: self.$adminID).frame(width: 150)
+                        SecureField("기본 관계자 비밀번호 입력", text: self.$adminPW).frame(width: 150)
                     }
                 }
                 VStack {
                     if self.UserDB.auth {
                         Button(action: {
-                            if !self.adminID.isEmpty {
-                                self.UserDB.adminID = self.adminID
-                            }
                             if !self.adminPW.isEmpty {
-                                self.UserDB.adminPW = self.adminPW
+                                if self.adminPW == self.captchaInput {
+                                    self.UserDB.adminPW = self.adminPW
+                                    UserDefaults.standard.set(self.UserDB.adminPW, forKey: "adminPW")
+                                    if !self.adminID.isEmpty {
+                                        self.UserDB.adminID = self.adminID
+                                        UserDefaults.standard.set(self.UserDB.adminID, forKey: "adminID")
+                                    }
+                                    self.UserDB.auth = false
+                                    self.UserDB.status = "TestOFSelect"
+                                } else {
+                                    self.isAlert = true
+                                    self.title = "비밀번호와 검증이 다릅니다. "
+                                }
+                            } else if !self.adminID.isEmpty {
+                                self.UserDB.adminID = self.adminID
+                                UserDefaults.standard.set(self.UserDB.adminID, forKey: "adminID")
+                                self.UserDB.auth = false
+                                self.UserDB.status = "TestOFSelect"
                             }
-                            UserDefaults.standard.set(self.UserDB.adminID, forKey: "adminID")
-                            UserDefaults.standard.set(self.UserDB.adminPW, forKey: "adminPW")
-                            self.UserDB.auth = false
-                            self.UserDB.status = "TestOFSelect"
-                        }) { Text("변경") }.padding().disabled(self.adminID.isEmpty && self.adminPW.isEmpty)
+                        }) { Text("변경") }.padding().disabled(self.adminID.isEmpty && (self.adminPW.isEmpty || self.captchaInput.isEmpty)).alert(isPresented: self.$isAlert) {
+                            Alert(title: Text(self.title), message: Text(self.message), dismissButton: .default(Text("승인"), action: {
+                                self.captchaInput = ""
+                                self.adminPW = ""
+                            }))
+                        }
                     } else if !self.UserDB.auth {
                         Button(action: {
                             self.captcha = arc4random_uniform(8999) + 1000
                         }) { Text("새로고침") }.padding()
                         Button(action: {
-                            if UInt(self.adminID) == UInt(self.captcha) && UInt(self.adminID) != nil {
+                            if UInt(self.captchaInput) == UInt(self.captcha) && UInt(self.captchaInput) != nil && self.adminID == "admin" && self.adminPW == "passwd" {
                                 self.UserDB.adminID = "admin"
                                 self.UserDB.adminPW = "passwd"
                                 self.UserDB.TestFile = nil
@@ -75,13 +96,21 @@ struct RAChange: View {
                                 UserDefaults.standard.removeObject(forKey: "userIDItem")
                                 UserDefaults.standard.removeObject(forKey: "answerItem")
                                 self.UserDB.status = "TestOFSelect"
+                            } else if UInt(self.captchaInput) == UInt(self.captcha) && UInt(self.captchaInput) != nil && (self.adminID != "admin" || self.adminPW != "passwd"){
+                                self.isAlert = true
+                                self.adminID = ""
+                                self.adminPW = ""
+                                self.title = "관계자 아이디 또는 비밀번호가 다릅니다. "
+                                self.message = "초기 아이디와 비밀번호를 입력했는지 다시 확인하여 주십시오. "
+                                self.captcha = arc4random_uniform(8999) + 1000
                             } else {
                                 self.isAlert = true
+                                self.title = "보안코드를 올바르게 입력해 주세요."
                                 self.captcha = arc4random_uniform(8999) + 1000
                             }
-                        }) { Text("초기화") }.padding().disabled(self.adminID.isEmpty).alert(isPresented: self.$isAlert) {
-                            Alert(title: Text("보안코드를 올바르게 입력해 주세요."), dismissButton: .default(Text("승인"), action: {
-                                self.adminID = ""
+                        }) { Text("초기화") }.padding().disabled(self.captchaInput.isEmpty || self.adminID.isEmpty || self.adminPW.isEmpty).alert(isPresented: self.$isAlert) {
+                            Alert(title: Text(self.title), message: Text(self.message), dismissButton: .default(Text("승인"), action: {
+                                self.captchaInput = ""
                             }))
                         }
                     }
@@ -91,6 +120,8 @@ struct RAChange: View {
                 Text("관계자 아이디 & 비밀번호 초기화는 즉시 적용되며")
                 Text("지정된 파일들은 정보 보호를 위해 설정이 초기화 됩니다. ")
             }
+            Text("").padding()
+            Text("만약 지원자가 관계자 계정을 변경 하였을 경우에는 회사가 지원자를 불합격 처리할 수 있습니다. ").foregroundColor(.purple)
             Spacer()
             Button(action: {
                 self.UserDB.auth = false
