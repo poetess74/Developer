9a1e4e53dc56db9c880903db9e8db59372ba82f9
HyeongminKim <assa0620@gmail.com>
2020-02-20 22:12:26 +0900

    modified:   Apple/Mac/SwiftUI/Coronavirus/Coronavirus.xcodeproj/project.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	modified:   Apple/Mac/SwiftUI/Coronavirus/Coronavirus/ContentView.swift

--- Apple/Mac/SwiftUI/Coronavirus/Coronavirus/ContentView.swift
+++ Apple/Mac/SwiftUI/Coronavirus/Coronavirus/ContentView.swift
@@ -34,7 +34,7 @@ struct ContentView: View {
                                 Button(action: {
                                     UIApplication.shared.open(self.callURL as URL, options: [:], completionHandler: nil)
                                 }, label: { Text("1339") })
-                                Text("에 연락 후 선별진료소 방문")
+                                Text("에 연락 후 상담사 지시에 따르기")
                             }
                         }
                     } else if !((self.fever || self.cough) || (self.sputum || self.lethargy) || (self.throat || self.dyspnea)) && self.done {
@@ -125,19 +125,19 @@ struct ContentView: View {
                         }.padding()
                     }
                 }.listStyle(GroupedListStyle())
-                if !self.done {
-                    Text("본 자가진단앱은 코로나 19에 감염되었는지\n간단히 체크하는 용도이며\n과신할 경우 문제의 소지가 있습니다.  ").multilineTextAlignment(.center)
+                if !((self.fever || self.cough) || (self.sputum || self.lethargy) || (self.throat || self.dyspnea)) || !self.done {
+                    Text("본 자가진단앱은 코로나 19에 감염되었는지 간단히 체크하는 용도이며 과신할 경우 문제의 소지가 있습니다.  ").multilineTextAlignment(.center).padding()
                 } else {
-                    Text("상담할 때 증상을 사실대로 의료진에게 얘기하셔야 하며\n의료진의 격리 및 치료 요구의 불응은 자신\n더 나아가 가족과 사회에\n큰 해가 될 수 있음을 숙지하십시오. ").multilineTextAlignment(.center).foregroundColor(.red)
+                    Text("상담할 때 증상을 사실대로 의료진에게 얘기하셔야 하며 의료진의 격리 및 치료 요구의 불응은 자신 더 나아가 가족과 사회에 큰 해가 될 수 있음을 숙지하십시오.\n코로나 19 검진으로 인해 병원을 방문할 시 \"선별 진료소\"로 자차를 이용하여 방문하시길 부탁드립니다. ").multilineTextAlignment(.center).foregroundColor(.red).padding()
                 }
                 if !self.done {
                     Button(action: {
                         if (self.fever || self.cough) || (self.sputum || self.lethargy) || (self.throat || self.dyspnea) {
                             self.title = "코로나 19 의심 증상이 확인되었습니다. "
-                            self.message = "1339에 연락 후\n가까운 선별 진료소에 방문하셔서\n진단을 받아보시길 권장드립니다. "
+                            self.message = "1339에 연락 후 가까운 선별 진료소에 방문하셔서 진단을 받아보시길 권장드립니다. "
                         } else {
                             self.title = "코로나 19에 감염되지 않았거나 잠복기인 것 같습니다. "
-                            self.message = "마스크, 손씻기 등\n위생관리를 철저히 하시길 바랍니다. "
+                            self.message = "마스크, 손씻기 등 위생관리를 철저히 하시길 바랍니다. "
                         }
                         self.isAlert = true
                         self.done = true
