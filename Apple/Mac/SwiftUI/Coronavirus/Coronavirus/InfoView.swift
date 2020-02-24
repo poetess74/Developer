0b08376af1a72d5ce9fcd059ad4fee7a9ee65c52
HyeongminKim <assa0620@gmail.com>
2020-02-24 14:42:13 +0900

    modified:   Apple/Mac/SwiftUI/Coronavirus/Coronavirus.xcodeproj/project.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	modified:   Apple/Mac/SwiftUI/Coronavirus/Coronavirus/InfoView.swift

--- Apple/Mac/SwiftUI/Coronavirus/Coronavirus/InfoView.swift
+++ Apple/Mac/SwiftUI/Coronavirus/Coronavirus/InfoView.swift
@@ -9,14 +9,26 @@
 import SwiftUI
 
 struct InfoView: View {
+    @State private var swapDevtoVer = false
+    @Environment(\.colorScheme) var colorSchme: ColorScheme
     var body: some View {
         NavigationView {
             List {
                 Section {
-                    HStack {
-                        Text("개발자")
-                        Spacer()
-                        Text("Hyeongmin Kim").foregroundColor(.gray)
+                    Button(action: {
+                        self.swapDevtoVer.toggle()
+                    }) {
+                        HStack {
+                            if !swapDevtoVer {
+                                colorSchme == .light ? Text("개발자").foregroundColor(.black) : Text("개발자").foregroundColor(.white)
+                                Spacer()
+                                Text("Hyeongmin Kim").foregroundColor(.gray)
+                            } else {
+                                colorSchme == .light ? Text("버전").foregroundColor(.black) : Text("버전").foregroundColor(.white)
+                                Spacer()
+                                Text("\(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "N/A")").foregroundColor(.gray)
+                            }
+                        }
                     }
                     NavigationLink(destination: DetailView(discipline: "출처 및 참고자료")) {
                         Text("출처 및 참고자료")
