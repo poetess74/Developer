4d808edaaf4344563cd1308cb066488f366a0d55
HyeongminKim <assa0620@gmail.com>
2020-02-24 18:28:49 +0900

    modified:   Apple/Mac/SwiftUI/Coronavirus/Coronavirus.xcodeproj/project.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	modified:   Apple/Mac/SwiftUI/Coronavirus/Coronavirus/DetailView.swift

--- Apple/Mac/SwiftUI/Coronavirus/Coronavirus/DetailView.swift
+++ Apple/Mac/SwiftUI/Coronavirus/Coronavirus/DetailView.swift
@@ -16,12 +16,21 @@ struct DetailView: View {
             if discipline == "판별 기준" {
                 VStack {
                     Image("ScreenShotScan").resizable().frame(width: 293, height: 222)
-                    Text("\"코로나 유행지역 방문 또는 확진자의 동선 겹침\"\n이 선택되어야 함").multilineTextAlignment(.center).padding()
+                    Text("")
+                    HStack {
+                        Rectangle()
+                            .fill(Color.blue)
+                            .frame(width: 20, height: 20, alignment: .center)
+                            .cornerRadius(5)
+                        Text("코로나 유행지역 방문 또는 확진자의 동선 겹침").font(.caption)
+                    }.padding()
+                    Text("위 사항이 선택되어야 함")
                 }
             } else if discipline == "주의 사항" {
-                Text("본 자가진단앱은 코로나 19에 감염되었는지 간단히 체크하는 용도이며 의료진의 처방보다 과신할 경우 문제의 소지가 있습니다.").multilineTextAlignment(.center).padding().foregroundColor(.red)
-                Text("상담할 때 증상을 사실대로 의료진에게 얘기하셔야 하며 의료진의 격리 및 치료 요구의 불응은 자신과 가족 더 나아가 사회에 큰 해가 될 수 있음을 숙지하십시오.").multilineTextAlignment(.center).padding().foregroundColor(.red)
-                Text("코로나 19 검진으로 인해 병원을 방문할 시 \"선별 진료소\"로 자차를 이용하여 방문하시길 부탁드립니다.").multilineTextAlignment(.center).padding().foregroundColor(.red)
+                Text("본 자가진단앱은 코로나 19에 감염되었는지 간단히 체크하는 용도이며 의료진의 처방보다 과신할 경우 문제의 소지가 있습니다.").multilineTextAlignment(.center).padding().foregroundColor(.red).fixedSize(horizontal: false, vertical: true)
+                Text("상담할 때 증상을 사실대로 의료진에게 얘기하셔야 하며 의료진의 격리 및 치료 요구의 불응은 자신과 가족 더 나아가 사회에 큰 해가 될 수 있음을 숙지하십시오.").multilineTextAlignment(.center).padding().foregroundColor(.red).fixedSize(horizontal: false, vertical: true)
+                Text("코로나 19 검진으로 인해 병원을 방문할 시 \"선별 진료소\"로 자차를 이용하여 방문하시길 부탁드립니다.").multilineTextAlignment(.center).padding().foregroundColor(.red).fixedSize(horizontal: false, vertical: true)
+                Text("이 앱의 일부 기능은 인터넷 연결을 필요로 합니다. ").padding()
             } else if discipline == "Q&A" {
                 WebView(request: URLRequest(url: URL(string: "http://ncov.mohw.go.kr/faqBoardList.do?brdId=3&brdGubun=34&dataGubun=&ncvContSeq=&contSeq=&board_id=")!))
             } else if discipline == "팩트 체크" {
