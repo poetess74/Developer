73c84543c1fb15f24921f034c58ba6d481f3b12c
HyeongminKim <assa0620@gmail.com>
2020-03-04 15:35:09 +0900

    added: Round04_01.java added: Round04_02.java added: Round04_Ex05.java added: Round04_Ex06.java added: Round04_Ex07.java added: Round04_Ex08.java added: Round04_Ex09.java

---
+++ Java/熱血講義/src/Round04/Round04_Ex05.java
@@ -0,0 +1,10 @@
+package Round04;
+
+import java.io.IOException;
+
+public class Round04_Ex05 {
+    public static void main(String[] args) throws IOException {
+        byte[] bb = {'J', 'A', 'V', 'A'};
+        System.out.write(bb);
+    }
+}
---
+++ Java/熱血講義/src/Round04/Round04_Ex06.java
@@ -0,0 +1,9 @@
+package Round04;
+
+public class Round04_Ex06 {
+    public static void main(String[] args) throws java.io.IOException {
+        System.out.print("입력 = ");
+        int aa = System.in.read();
+        System.out.println("입력된 아스키 값은 " + aa + "입니다. ");
+    }
+}
