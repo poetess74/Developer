76e84e851a78e6bf59b44f6984a59332ea8821c3
HyeongminKim <assa0620@gmail.com>
2020-03-14 17:44:08 +0900

    added: Round15_Ex09 added: Round15_Ex10 added: Round15_Ex11 added: Ex12_Ex13/Round15_Ex12 added: Ex12_Ex13/Round15_Ex13 added: Round15_Ex14 added: Round15_Ex15

---
+++ Java/熱血講義/src/Part02/Round15/Ex12_Ex13/Round15_Ex12.java
@@ -0,0 +1,8 @@
+package Part02.Round15.Ex12_Ex13;
+
+public class Round15_Ex12 {
+    protected Round15_Ex12() { }
+    protected void disp() {
+        System.out.println("Package로 만든 클래스");
+    }
+}
---
+++ Java/熱血講義/src/Part02/Round15/Round15_Ex09.java
@@ -0,0 +1,32 @@
+package Part02.Round15;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Round15_Ex09 {
+    protected static int aaa() {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int age = 0;
+        try {
+            System.out.print("나이 = ");
+            age = Integer.parseInt(in.readLine());
+            return age;
+        } catch (NumberFormatException nfe) {
+            System.out.println("error = " + nfe.getMessage());
+            return -1;
+        } catch (IOException ie) {
+            return -1;
+        } finally {
+            System.out.println("나이 : " + age);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main Start!!");
+        aaa();
+        System.out.println("Main End!!");
+    }
+}
