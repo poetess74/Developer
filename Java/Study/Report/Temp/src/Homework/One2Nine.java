3949899d3c0f7b9134b1a6875c301d99066984a1
HyeongminKim <assa0620@gmail.com>
2020-03-17 14:45:55 +0900

    modified:   Java/Study/Report/Temp/.idea/misc.xml 	modified:   Java/Study/Report/Temp/.idea/modules.xml 	modified:   Java/Study/Report/Temp/src/Homework/One2Nine.java 	modified:   Java/Study/Report/Temp/src/Homework/Sorry.java

--- Java/Study/Report/Temp/src/Homework/One2Nine.java
+++ Java/Study/Report/Temp/src/Homework/One2Nine.java
@@ -1,4 +1,10 @@
 package Homework;
 
 public class One2Nine {
+    public static void main(String[] args) {
+        for (int i = 0; i < 9; i++) {
+            System.out.print((i + 1) + " ");
+        }
+        System.out.println();
+    }
 }
--- Java/Study/Report/Temp/src/Homework/Sorry.java
+++ Java/Study/Report/Temp/src/Homework/Sorry.java
@@ -1,4 +1,7 @@
 package Homework;
 
 public class Sorry {
+    public static void main(String[] args) {
+        System.out.println("Sorry~~");
+    }
 }
