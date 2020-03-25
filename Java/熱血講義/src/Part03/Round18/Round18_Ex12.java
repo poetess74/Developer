77b91591eaa226c88a33050d94f5f9b9e3f77150
HyeongminKim <assa0620@gmail.com>
2020-03-25 18:43:34 +0900

    modified:   Apple/iOS/SwiftUI/Coronavirus/Coronavirus.xcodeproj/project.xcworkspace/xcuserdata/hm.xcuserdatad/UserInterfaceState.xcuserstate 	modified:   "Java/\347\206\261\350\241\200\350\254\233\347\276\251/.idea/workspace.xml" 	modified:   "Java/\347\206\261\350\241\200\350\254\233\347\276\251/src/Part03/Round18/Round18_Ex12.java"

--- Java/熱血講義/src/Part03/Round18/Round18_Ex12.java
+++ Java/熱血講義/src/Part03/Round18/Round18_Ex12.java
@@ -12,9 +12,8 @@ class Round18_Ex12_Sub extends Frame {
     private Dimension dimension, dimension1;
     private int xpos, ypos;
     private Label label = new Label("문자열 입력");
-    private TextField textField = new TextField(30);
     protected Round18_Ex12_Sub() {
-        super("문자열");
+        super();
         this.init();
         this.start();
         this.setSize(300, 200);
@@ -27,23 +26,9 @@ class Round18_Ex12_Sub extends Frame {
         try {
             Thread.sleep(5000);
         } catch (InterruptedException ee) { }
-        String string = textField.getText().trim();
-        textField.setEchoChar((char)0);
-        textField.setText(string);
     }
     protected void init() {
-        GridBagLayout gridBagLayout = new GridBagLayout();
-        GridBagConstraints gridBagConstraints = new GridBagConstraints();
-        this.setLayout(gridBagLayout);
-        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.weightx = 4;
-        gridBagLayout.setConstraints(label, gridBagConstraints);
-        this.add(label);
-        gridBagConstraints.weightx = 6;
-        gridBagLayout.setConstraints(textField, gridBagConstraints);
-        this.add(textField);
-        this.add(textField);
-        textField.setEchoChar('*');
+
     }
     protected void start() { }
 }
