0db6bad18db17da89699089aa172e7d827a8531c
HyeongminKim <assa0620@gmail.com>
2020-03-31 16:36:40 +0900

    modified: Round18_Ex15

--- Java/熱血講義/src/Part03/Round18/Round18_Ex15.java
+++ Java/熱血講義/src/Part03/Round18/Round18_Ex15.java
@@ -16,6 +16,23 @@ class Round18_Ex15_Sub extends Frame {
     private Menu edit = new Menu("편집");
     private Menu view = new Menu("보기");
     private Menu help = new Menu("도움말");
+    private MenuItem fnew = new MenuItem("새 파일");
+    private MenuItem fopen = new Menu("열기");
+    private MenuItem fsave = new Menu("저장");
+    private MenuItem fsaveas = new MenuItem("다른 이름으로 저장");
+    private MenuItem fexit = new MenuItem("종료");
+    private MenuItem esearch = new MenuItem("찾기");
+    private MenuItem echange = new MenuItem("바꾸기");
+    private MenuItem emove = new MenuItem("이동");
+    private Menu vmo = new Menu("모양");
+    private MenuItem vmbasic = new MenuItem("기본 모양");
+    private MenuItem vmweb = new MenuItem("웹 모양");
+    private MenuItem vmprint = new MenuItem("인쇄 모양");
+    private Menu vtool = new Menu("도구 모음");
+    private MenuItem hver = new Menu("버전 정보");
+    private CheckboxMenuItem vtcustom = new CheckboxMenuItem("표준", true);
+    private CheckboxMenuItem vtform = new CheckboxMenuItem("서식", true);
+    private CheckboxMenuItem vtexam = new CheckboxMenuItem("검토", false);
     protected Round18_Ex15_Sub() {
         super("메뉴 만들기");
         this.init();
@@ -30,6 +47,25 @@ class Round18_Ex15_Sub extends Frame {
     }
     protected void init() {
         this.setMenuBar(menuBar);
+        file.add(fnew);
+        file.addSeparator();
+        file.add(fopen);
+        file.add(fsave);
+        fsaveas.setEnabled(false);
+        file.add(fsaveas);
+        file.add(fexit);
+        edit.add(esearch);
+        edit.add(echange);
+        edit.add(emove);
+        view.add(vmo);
+        vmo.add(vmbasic);
+        vmo.add(vmweb);
+        vmo.add(vmprint);
+        view.add(vtool);
+        help.add(hver);
+        vtool.add(vtcustom);
+        vtool.add(vtform);
+        vtool.add(vtexam);
         menuBar.add(file);
         menuBar.add(edit);
         menuBar.add(view);
