48280710d22bd3c7d5f56d9fa7a59b4cd2e82457
HyeongminKim <assa0620@gmail.com>
2020-04-13 12:43:00 +0900

    modified: ContentView

--- Apple/Mac/SwiftUI/Timer/Timer/ContentView.swift
+++ Apple/Mac/SwiftUI/Timer/Timer/ContentView.swift
@@ -8,6 +8,8 @@
 
 import SwiftUI
 
+var now = Date()
+
 struct ContentView: View {
     @State var setTimer = false
     @State var setTime = 0
@@ -19,19 +21,42 @@ struct ContentView: View {
                 Text("타이머 설정").bold().padding().fixedSize().font(.largeTitle)
                 Text(convertTime(inputTime: self.startTime, dotEnable: true)).font(.title).foregroundColor(.gray)
                 HStack {
-                    Button(action: { self.startTime += 60 }, label: { Text("+1분") }).disabled(self.startTime + 60 >= 86400)
-                    Button(action: { self.startTime += 300 }, label: { Text("+5분") }).disabled(self.startTime + 300 >= 86400)
-                    Button(action: { self.startTime += 600 }, label: { Text("+10분") }).disabled(self.startTime + 600 >= 86400)
+                    Button(action: {
+                        self.startTime += 60
+                        now = Date()
+                    }, label: { Text("+1분") }).disabled(self.startTime + 60 >= 86400)
+                    Button(action: {
+                        self.startTime += 300
+                        now = Date()
+                    }, label: { Text("+5분") }).disabled(self.startTime + 300 >= 86400)
+                    Button(action: {
+                        self.startTime += 600
+                        now = Date()
+                    }, label: { Text("+10분") }).disabled(self.startTime + 600 >= 86400)
                 }
                 HStack {
-                    Button(action: { self.startTime -= 60 }, label: { Text("-1분") }).disabled(self.startTime - 60 < 0)
-                    Button(action: { self.startTime -= 300 }, label: { Text("-5분") }).disabled(self.startTime - 300 < 0)
-                    Button(action: { self.startTime -= 600 }, label: { Text("-10분") }).disabled(self.startTime - 600 < 0)
+                    Button(action: {
+                        self.startTime -= 60
+                        now = Date()
+                    }, label: { Text("-1분") }).disabled(self.startTime - 60 < 0)
+                    Button(action: {
+                        self.startTime -= 300
+                        now = Date()
+                    }, label: { Text("-5분") }).disabled(self.startTime - 300 < 0)
+                    Button(action: {
+                        self.startTime -= 600
+                        now = Date()
+                    }, label: { Text("-10분") }).disabled(self.startTime - 600 < 0)
+                }
+                HStack {
+                    Text("ETA: ")
+                    self.startTime == 0 ? Text("N/A") : Text(convertDate(inputTime: self.startTime, secEnable: false))
                 }
                 HStack {
                     Button(action: {
                         self.setTimer = true
                         self.setTime = self.startTime
+                        now = Date()
                         self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
                             guard self.startTime > 0 else {
                                 self.timer?.invalidate()
@@ -43,7 +68,11 @@ struct ContentView: View {
                     Button(action: { self.startTime = 0 }, label: { Text("초기화")}).disabled(self.startTime == 0)
                 }
             } else {
-                Text("타이머 시작").bold().padding().fixedSize().font(.largeTitle)
+                if self.startTime != 0 {
+                    Text("타이머 시작").bold().padding().fixedSize().font(.largeTitle)
+                } else {
+                    Text("타이머 완료").bold().padding().fixedSize().font(.largeTitle)
+                }
                 if self.startTime % 2 == 0 {
                     if 32 > self.startTime && self.startTime > 24 {
                         Text(convertTime(inputTime: self.startTime, dotEnable: true)).font(.title).foregroundColor(.red)
@@ -60,11 +89,15 @@ struct ContentView: View {
                     }
                 }
                 HStack {
+                    Text("ETA: ")
+                    Text(convertDate(inputTime: self.setTime, secEnable: true))
+                }
+                HStack {
                     Button(action: {
                         self.setTimer = false
                         self.startTime = self.setTime
                         self.timer?.invalidate()
-                    }, label: { Text("정지") })
+                    }, label: { self.startTime != 0 ? Text("정지") : Text("이전") })
                     Button(action: {
                         self.setTimer = false
                         self.startTime = 0
@@ -86,6 +119,18 @@ private func convertTime(inputTime: Int, dotEnable: Bool) -> String {
     } else {
         return String(format: "%02i %02i %02i", hour, min, sec)
     }
+}
+
+private func convertDate(inputTime: Int, secEnable: Bool) -> String {
+    let date = DateFormatter()
+    let convertedTime = now + TimeInterval(inputTime) as Date
+    if secEnable {
+        date.dateFormat = "HH:mm:ss"
+    } else {
+        date.dateFormat = "HH:mm"
+    }
+        
+    return String(date.string(from: convertedTime))
 }
 
 struct ContentView_Previews: PreviewProvider {
