7362bee71338239ea96e21afbfb1c8887d09ca97
hm <assa0620@gmail.com>
2020-05-01 13:34:01 +0900

    new file:   changesu.sh

---
+++ Shellscript/changesu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$1" == "brew" ]; then
+    su bard -c "brew $@"
+elif [ "$1" == "brewall" ]; then
+    su bard -c "/Users/bard/Library/Scripts/brewall.sh"
+elif [ "$1" == "rtlogin" ]; then
+    su bard -c "/Users/bard/Library/Scripts/rtlogin.sh $2"
+fi
