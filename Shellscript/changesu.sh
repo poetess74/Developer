e8006ac35154f56599066792bc8c122c3f491216
hm <assa0620@gmail.com>
2020-05-07 21:22:02 +0900

    modified:   Shellscript/changesu.sh

--- Shellscript/changesu.sh
+++ Shellscript/changesu.sh
@@ -3,7 +3,7 @@
 if [ "$1" == "brew" ]; then
     su bard -c "brew $@"
 elif [ "$1" == "brewall" ]; then
-    su bard -c "/Users/bard/Library/Scripts/brewall.sh"
+    su bard -c "/Users/bard/Library/Scripts/brewall.sh; echo -n \"Installed brewall script version: \"; /Users/bard/Library/Scripts/brewall.sh version"
 elif [ "$1" == "rtlogin" ]; then
     su bard -c "/Users/bard/Library/Scripts/rtlogin.sh $2"
 fi
