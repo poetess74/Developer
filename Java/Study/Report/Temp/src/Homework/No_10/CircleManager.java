dec2127aa925d1baf3ee3e24d26a7c17b8a63148
HyeongminKim <assa0620@gmail.com>
2020-06-04 11:34:43 +0900

    added: CircleManager added: ShapeApp

---
+++ Java/Study/Report/Temp/src/Homework/No_10/CircleManager.java
@@ -0,0 +1,28 @@
+package Homework.No_10;
+
+class CircleManager_Circle {
+    private int x, y, radius;
+    public CircleManager_Circle(int x, int y, int radius) {
+        this.x = x; this.y = y; this.radius = radius;
+    }
+    public String toString() {
+        return "(" + x + ", " + y + ") 반지름 " + radius;
+    }
+    public double area() {
+        return radius * radius * 3.14;
+    }
+    public boolean equals(Object object) {
+        CircleManager_Circle circle = (CircleManager_Circle)object;
+        return area() == circle.area();
+    }
+}
+
+public class CircleManager {
+    public static void main(String[] args) {
+        CircleManager_Circle a = new CircleManager_Circle(1, 2, 10);
+        CircleManager_Circle b = new CircleManager_Circle(5, 6, 10);
+        System.out.println("원 1: " + a);
+        System.out.println("원 2: " + b);
+        System.out.println(a.equals(b) ? "같은 원입니다. " : "다른 원입니다. ");
+    }
+}
---
+++ Java/Study/Report/Temp/src/Homework/No_10/ShapeApp.java
@@ -0,0 +1,28 @@
+package Homework.No_10;
+
+interface ShapeApp_Shape {
+    final float PI = 3.14f;
+    void draw();
+    float getArea();
+    default public void redraw() {
+        System.out.println("--- 다시 그립니다. ---");
+        draw();
+    }
+}
+
+class ShapeApp_Circle implements ShapeApp_Shape {
+    private int radius;
+    public void draw() {
+        System.out.printf("반지름 %d ", radius, getArea());
+    }
+    public float getArea() { return radius * radius * PI; }
+    public ShapeApp_Circle(int radius) { this.radius = radius; }
+}
+
+public class ShapeApp {
+    public static void main(String[] args) {
+        ShapeApp_Shape coin = new ShapeApp_Circle(10);
+        coin.redraw();
+        System.out.printf("코인의 면적은 %.1f", coin.getArea());
+    }
+}
