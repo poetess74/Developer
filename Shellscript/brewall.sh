5e7475224e7936433de9d505a6342c8ef66564a0
HyeongminKim <assa0620@gmail.com>
2020-06-29 13:46:30 +0900

    modified:   Shellscript/brewall.sh

--- Shellscript/brewall.sh
+++ Shellscript/brewall.sh
@@ -6,8 +6,8 @@ update=false
 upgrade=false
 cleanup=false
 doctor=false
-version=1.0.6
-build=1A008
+version=1.0.7
+build=1A012
 
 function printInit() {
     if [ $LANG == "ko_KR.UTF-8" ]; then
@@ -32,9 +32,9 @@ function printInit() {
         echo 'TODO: INSERT this command to your $SHELL_CONFIG_FILE (except "#!>", "<!#"). '
         echo 'NOTE: This command tested on zsh.'
         echo ''
-        echo '#!> echo 'alias brewall="$CUSTOMPATH/brewall.sh;softwareupdate -l;echo \"\a\""' >> $SHELL_CONFIG_FILE && source $SHELL_CONFIG_FILE; chmod 550 $CUSTOMPATH/brewall.sh; touch ~/Library/Application\ Support/com.greengecko.brewall.initializationed <!#'
+        echo '#!> echo 'alias brewall="$CUSTOMPATH/brewall.sh;softwareupdate -l;echo \"\a\""' >> $SHELL_CONFIG_FILE && source $SHELL_CONFIG_FILE; chmod 550 $CUSTOMPATH/brewall.sh; mkdir ~/Library/Application\ Support/com.greengecko.brewall; touch ~/Library/Application\ Support/com.greengecko.brewall/initializationed <!#'
         echo ''
-        echo 'ex) echo 'alias brewall="~/.etc/brewall.sh;upgrade_oh_my_zsh;softwareupdate -l;echo \"\a\""' >> ~/.zshrc && source ~/.zshrc; chmod 550 ~/.etc/brewall.sh; touch ~/Library/Application\ Support/com.greengecko.brewall.initializationed'
+        echo 'ex) echo 'alias brewall="~/.etc/brewall.sh;upgrade_oh_my_zsh;softwareupdate -l;echo \"\a\""' >> ~/.zshrc && source ~/.zshrc; chmod 550 ~/.etc/brewall.sh; mkdir ~/Library/Application\ Support/com.greengecko.brewall; touch ~/Library/Application\ Support/com.greengecko.brewall/initializationed'
         echo ''
         echo 'WARN: Do not modify ">>" to ">". You will lost your $SHELL_CONFIG_FILE forever.'
         echo '      Do not grant write permission all user account. (except path change) '
@@ -47,7 +47,7 @@ function printInit() {
     fi
 }
 
-ls ~/Library/Application\ Support 2>/dev/null | grep com.greengecko.brewall.initializationed > /dev/null 2>&1
+ls ~/Library/Application\ Support/com.greengecko.brewall 2>/dev/null | grep initializationed > /dev/null 2>&1
 if [ "$?" != "0" ]; then
     inited=false
     if [ $LANG == "ko_KR.UTF-8" ]; then
@@ -58,6 +58,19 @@ if [ "$?" != "0" ]; then
         echo " WARN: Please add \"init\" option to run faster this script."
         echo "USAGE: ./brewall.sh init"
         echo " INFO: This option will print install guide."
+    fi
+    which brew > /dev/null 2>&1
+    if [ $? != 0 ]; then
+        if [ $LANG == "ko_KR.UTF-8" ]; then
+            echo "이 brewall 스크립트는 Homebrew 패키지 관리자를 더 사용하기 쉽도록 하는 도구이며 Homebrew가 필수적으로 필요합니다. "
+            echo -e "\033[0;1mhttps://brew.sh/index_ko\033[m 이 사이트에 들어가서 Homebrew를 설치하는 것을 도움받거나"
+            echo "아니면 아래 명령어를 쉘에 붙여넣으세요. (이 스크립트는 무엇을 할지 설명하고 실행하기 전 잠시 대기합니다. )"
+        else
+            echo "This brewall script require Homebrew. Because extend of Homebrew tools."
+            echo -e "Please enter this site \033[0;1mhttps://brew.sh\033[m and support while install Homebrew or "
+            echo "Below paste command in the shell. (This script explains what it will do and then pauses before it does it. )"
+        fi
+        echo '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"'
     fi
 fi
 
@@ -85,7 +98,7 @@ else
     fi
 fi
 
-ls ~/Library/Logs/Homebrew > /dev/null 2>&1
+ls $debugPath > /dev/null 2>&1
 if [ "$?" != "0" ]; then
     mkdir ~/Library/Logs/Homebrew
     if [ $LANG == "ko_KR.UTF-8" ]; then
@@ -95,7 +108,40 @@ if [ "$?" != "0" ]; then
     fi
 fi
 
-ls $debugPath |grep brewall_initiated 
+ls ~/Library/Application\ Support/com.greengecko.brewall 2> /dev/null | grep $version > /dev/null 2>&1
+if [ $? != 0 ]; then
+    shasum -a 256 $0 > ~/Library/Application\ Support/com.greengecko.brewall/$version.csm 2> /dev/null
+    if [ $LANG == "ko_KR.UTF-8" ]; then
+        echo -n "현재 스크립트 체크섬: "
+    else
+        echo -n "Current script checksum: "
+    fi
+    cat ~/Library/Application\ Support/com.greengecko.brewall/$version.csm 
+else
+    shasum -a 256 $0 > $debugPath/$version.csm
+    diff ~/Library/Application\ Support/com.greengecko.brewall/$version.csm $debugPath/$version.csm > /dev/null 2>&1
+    if [ $? == 0 ]; then
+        echo "" > /dev/null 2>&1
+    else
+        if [ $LANG == "ko_KR.UTF-8" ]; then
+            echo -n "저장된 스크립트 체크섬: "
+            cat ~/Library/Application\ Support/com.greengecko.brewall/$version.csm 2> /dev/null
+            echo -n "현재 스크립트 체크섬: "
+            cat $debugPath/$version.csm
+            echo "스크립트 파일이 악의적 목적으로 변조되었을 가능성이 있어 중단되었습니다. "
+        else
+            echo -n "Saved script checksum: "
+            cat ~/Library/Application\ Support/com.greengecko.brewall/$version.csm 2> /dev/null
+            echo -n "Current script checksum: "
+            cat $debugPath/$version.csm
+            echo "Unauthorized edited script who changes by hacker. Aborting."
+        fi
+        exit 1
+    fi
+    rm $debugPath/$version.csm
+fi
+
+ls $debugPath 2> /dev/null |grep brewall_initiated > /dev/null 2>&1
 if [ $? == 0 ]; then
     cat $debugPath/brewall_initiated.log
 fi
