b9f3b3dcf833b8ee93dcb040d2d0f62004025921
HyeongminKim <assa0620@gmail.com>
2020-09-03 17:58:34 +0900

    Added: Inputer

---
+++ Java/Study/Report/src/Second_Semester/No_1/Inputer.java
@@ -0,0 +1,21 @@
+package Second_Semester.No_1;
+
+import java.util.Scanner;
+
+public class Inputer {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int sum = 0;
+		System.out.print("두 정수를 입력하세요>> ");
+		int[] numbers = new int[2];
+		for(int i = 0; i < numbers.length; i++) {
+			numbers[i] = scanner.nextInt();
+		}
+		for(int i = 0; i < numbers.length; i++) {
+			sum += numbers[i];
+			System.out.print(numbers[i]);
+			System.out.print(" + ");
+		}
+		System.out.print(sum);
+	}
+}
---
+++ Unity/Practice/FlappyBird/Assets/Scripts/UI/Deluxe/Number.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Number : MonoBehaviour {
+    [SerializeField] private GameObject digitPrintLocation;
+    
+    [SerializeField] private List<Sprite> smallDigit;
+    [SerializeField] private List<Sprite> midDigit;
+    [SerializeField] private List<Sprite> bigDigit;
+
+    [SerializeField] private List<GameObject> digits;
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        
+    }
+}
