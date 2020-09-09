652ce236aae4f411dc00d6a4bd59a1d07e6cfada
hm <assa0620@gmail.com>
2020-09-09 23:48:00 +0900

    Medal controller added and Result anim added pivot applied.

---
+++ Unity/Practice/FlappyBird/Assets/Scripts/InGame/MedalgrantController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MedalgrantController : MonoBehaviour {
+    [SerializeField] private GameObject goldMedal;
+    [SerializeField] private GameObject silverMedal;
+    private void Update() {
+        if(GamePlayManager.Score >= 5 && GamePlayManager.Score <= 9 && GamePlayManager.isBestScore) {
+            silverMedal.SetActive(true);
+        } else if(GamePlayManager.Score >= 10 && GamePlayManager.isBestScore) {
+            goldMedal.SetActive(true);
+        } else {
+            silverMedal.SetActive(false);
+            goldMedal.SetActive(false);
+        }
+    }
+}
--- Unity/Practice/FlappyBird/Assets/Scripts/UI/ScoreViewer.cs
+++ Unity/Practice/FlappyBird/Assets/Scripts/UI/ScoreViewer.cs
@@ -4,8 +4,17 @@ using UnityEngine.UI;
 public class ScoreViewer : MonoBehaviour {
     [SerializeField] private GameObject player;
     [SerializeField] private Image digit;
+    [SerializeField] private Camera mainCamera;
+
+    private Vector3 digitWorldPoint;
+
+    private void Start() {
+        digitWorldPoint = mainCamera.ScreenToWorldPoint(digit.transform.position);
+        Debug.Log("target height:" + digitWorldPoint.y);
+    }
+
     private void Update() {
-        if(player.transform.position.y > 3 && player.transform.position.y < 4) {
+        if(Mathf.Approximately(player.transform.position.y, digitWorldPoint.y)) {
             digit.color = new Color(1f, 1f, 1f, 0.5f);
         } else {
             digit.color = new Color(1f, 1f, 1f, 1f);
