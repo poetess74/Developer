2866861696a5658ece2186f91dd58fce2b7c7012
HyeongminKim <assa0620@gmail.com>
2020-09-12 00:45:50 +0900

    Score number animation function base made.

--- Unity/Practice/FlappyBird/Assets/Scripts/UI/Deluxe/ScoreController.cs
+++ Unity/Practice/FlappyBird/Assets/Scripts/UI/Deluxe/ScoreController.cs
@@ -6,9 +6,9 @@ public class ScoreController : MonoBehaviour {
     [SerializeField] private char digitSize;
     [SerializeField] private GameObject newRecord;
     private void Update() {
-        controlPans[0].Set((int) GamePlayManager.Score, digitSize);
+        controlPans[0].Set((int) GamePlayManager.Score, digitSize, true);
         if(controlPans.Count - 1 != 1) return;
-        controlPans[1].Set((int) GamePlayManager.bestScore, digitSize);
+        controlPans[1].Set((int) GamePlayManager.bestScore, digitSize, true);
         newRecord.SetActive(GamePlayManager.isBestScore);
     }
 }
