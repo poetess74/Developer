f93d3eb9fb46f90edc9049e88f802be02e0b457d
HyeongminKim <assa0620@gmail.com>
2020-09-12 16:46:09 +0900

    Player no damage function added.

--- Unity/Practice/FlappyBird/Assets/Scripts/InGame/PlayerController.cs
+++ Unity/Practice/FlappyBird/Assets/Scripts/InGame/PlayerController.cs
@@ -1,16 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class PlayerController : MonoBehaviour {
     [SerializeField] private List<GameObject> players;
     [SerializeField] private bool isControllable;
+    [SerializeField] private bool isDamageable;
     
     private float jumpSpeed = 30;
     private GameObject playerImage;
     private void Start() {
         if (isControllable)
             GamePlayManager.IsGameOver = false;
+        if(!isDamageable)
+            gameObject.GetComponent<Rigidbody2D>().freezeRotation = true;
         foreach(var player in players) {
             player.SetActive(false);
         }
@@ -25,14 +29,30 @@ public class PlayerController : MonoBehaviour {
             gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * jumpSpeed);
         }
 
-        if(Mathf.Abs(transform.position.x) >= Mathf.Abs(GamePlayManager.limitPosition)) {
-            Destroy(gameObject);
+        if(Mathf.Abs(transform.position.x) > Mathf.Abs(GamePlayManager.limitPosition)) {
+            if(isDamageable) {
+                Destroy(gameObject);
+            } else {
+                if(transform.position.y < -3.62f || transform.position.y > 4.86f) {
+                    gameObject.GetComponent<Rigidbody2D>().position = new Vector2(0, -1.8f);
+                } else {
+                    gameObject.GetComponent<Rigidbody2D>().position = new Vector2(0, transform.position.y);
+                }
+            }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        if(!other.transform.CompareTag("Respawn")) return;
-        GamePlayManager.IsGameOver = true;
-        playerImage.GetComponent<Animation>().Stop();
+        if(!isDamageable) {
+            if(other.transform.CompareTag("Finish")) {
+                GamePlayManager.IsGameOver = true;
+                playerImage.GetComponent<Animation>().Stop();
+            }
+        } else {
+            if(other.transform.CompareTag("Respawn") || other.transform.CompareTag("Finish")) {
+                GamePlayManager.IsGameOver = true;
+                playerImage.GetComponent<Animation>().Stop();
+            }
+        }
     }
 }
