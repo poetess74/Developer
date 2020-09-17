4d525666ecee9e3449262dc628e7718d50cb1d94
HyeongminKim <assa0620@gmail.com>
2020-09-17 23:10:54 +0900

    Player different status added [Still adding].

---
+++ Unity/Practice/DragonFlights/Assets/Scripts/InGame/Player/PlayerSpawnController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerSpawnController : MonoBehaviour {
+    [SerializeField] private GameObject[] players;
+    private GameObject activePlayers;
+    private void Start() {
+        foreach(var player in players) {
+            player.SetActive(false);
+        }
+        int activeIndex = Random.Range(0, players.Length);
+        activePlayers = players[activeIndex];
+        activePlayers.SetActive(true);
+    }
+}
