1c99016e7f1d5a331c533d7e84e817bbf137c9c2
HyeongminKim <assa0620@gmail.com>
2020-09-28 21:13:39 +0900

    modified:   MCU/ATmega/Report/First/LedRelay/my_idea.c 	modified:   MCU/ATmega/Report/First/LedRelay/my_idea.cof 	modified:   MCU/ATmega/Report/First/LedRelay/my_idea.lss 	modified:   MCU/ATmega/Report/First/LedRelay/my_idea.ly1 	modified:   MCU/ATmega/Report/First/LedRelay/my_idea.prj 	modified:   MCU/ATmega/Report/First/LedRelay/my_idea.sym

--- MCU/ATmega/Report/First/LedRelay/my_idea.c
+++ MCU/ATmega/Report/First/LedRelay/my_idea.c
@@ -1,43 +1,46 @@
 // ***********************************************************
-// Project:
-// Author:
-// Module description:
+// Project: First Report
+// Subject: LED Relay
+// Author: 20194315 Hyeongmin Kim
 // ***********************************************************
 
 #include <avr/io.h>
 #include <util/delay.h>
-#define INDEX_SIZE 9
+#define LED_CTRL_INDEX 9
 
 int main(void) {
+    /* Define ports and value */
     DDRC = 0xFF;
-    PORTC = 0X00;
-    char indexArray[INDEX_SIZE] = {  0b11111111, 0b11111110, 0b11111100, // 0xFF, 0xFE, 0xFC
-                                     0b11111000, 0b11110000, 0b11100000, // 0xF8, 0xF0, 0xE0
-                                     0b11000000, 0b10000000, 0b00000000  // 0xC0, 0x80, 0x00
-                                  };
     char index;
+    char indexArray[LED_CTRL_INDEX] = {  0b11111111, 0b11111110, 0b11111100, // 0xFF, 0xFE, 0xFC
+                                         0b11111000, 0b11110000, 0b11100000, // 0xF8, 0xF0, 0xE0
+                                         0b11000000, 0b10000000, 0b00000000  // 0xC0, 0x80, 0x00
+                                      };
+
+    /* Initial setting (All LED off) */
+    PORTC = 0XFF;
 
     while(1) {
         /* Use bit-shift operator */ 
         index = 0xFF;
-        for(int i = 0; i < INDEX_SIZE; i++) {
+        for(int i = 0; i < LED_CTRL_INDEX; i++) {
             PORTC = index;
             _delay_ms(100);
             index <<= 1;
         }
         index = 0xFF;
-        for(int i = 0; i < INDEX_SIZE; i++) {
+        for(int i = 0; i < LED_CTRL_INDEX; i++) {
             PORTC = ~index;
             _delay_ms(100);
             index >>= 1;
         }
 
         /* Use for-loop and array */
-        for(int i = 0; i < INDEX_SIZE; i++) {
+        for(int i = 0; i < LED_CTRL_INDEX; i++) {
             PORTC = indexArray[i];
             _delay_ms(100);
         }
-        for(int i = INDEX_SIZE - 1; i >= 0; i--) {
+        for(int i = LED_CTRL_INDEX - 1; i >= 0; i--) {
             PORTC = indexArray[i];
             _delay_ms(100);
         }
