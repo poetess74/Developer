e5f755497f21f703049bc37af94a078e57a700f8
hm <assa0620@gmail.com>
2020-10-27 18:26:33 +0900

    modified:   changesu.sh

--- Shellscript/changesu.sh
+++ Shellscript/changesu.sh
@@ -3,7 +3,7 @@
 if [ "$1" == "brew" ]; then
     su bard -c "brew $@"
 elif [ "$1" == "brewall" ]; then
-    su bard -c "/Users/bard/Library/Scripts/brewall.sh; echo -n \"Installed brewall script version: \"; /Users/bard/Library/Scripts/brewall.sh version"
+    su bard -c "/Users/bard/Library/Scripts/brewall/brewall.sh; cd /Users/bard/Library/Scripts/brewall; git pull origin master; cd - > /dev/null"
 elif [ "$1" == "rtlogin" ]; then
     su bard -c "/Users/bard/Library/Scripts/rtlogin.sh $2"
 fi
