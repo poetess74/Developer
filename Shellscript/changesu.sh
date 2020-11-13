13cfd9ac6f221ac0c9d5a86ae21bccedca16d7ba
hm <assa0620@gmail.com>
2020-11-13 18:07:45 +0900

    modified:   Shellscript/changesu.sh

--- Shellscript/changesu.sh
+++ Shellscript/changesu.sh
@@ -3,7 +3,7 @@
 if [ "$1" == "brew" ]; then
     su bard -c "brew $@"
 elif [ "$1" == "brewall" ]; then
-    su bard -c "/Users/bard/Library/Scripts/brewall/brewall.sh; cd /Users/bard/Library/Scripts/brewall; git pull origin master; cd - > /dev/null"
+    su bard -c "/Users/bard/Library/Scripts/brewall.sh"
 elif [ "$1" == "rtlogin" ]; then
     su bard -c "/Users/bard/Library/Scripts/rtlogin.sh $2"
 fi
