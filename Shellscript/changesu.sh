5e0c218eca2b24b754c6ac2c73181a6030d23c49
hm <assa0620@gmail.com>
2020-11-16 23:09:22 +0900

    modified:   Shellscript/changesu.sh

--- Shellscript/changesu.sh
+++ Shellscript/changesu.sh
@@ -3,14 +3,14 @@
 cd ~
 echo "Enter password for bard. "
 
-if [ "$1" == "brewall" ]; then
+if [ "$1" == "mysql.server" ]; then
+    su bard -c "mysql.server $2"
+    result=$?
+elif [ "$1" == "brewall" ]; then
     su bard -c "/Users/bard/Library/Scripts/brewall.sh"
     result=$?
 elif [ "$1" == "rtlogin" ]; then
     su bard -c "/Users/bard/Library/Scripts/rtlogin.sh $2"
-    result=$?
-else
-    su bard -c "$@"
     result=$?
 fi
 
