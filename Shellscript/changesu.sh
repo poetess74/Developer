6094d935269e2b7014a55d1ee2a5428f2ef63eb1
hm <assa0620@gmail.com>
2020-11-16 16:08:55 +0900

    modified:   Shellscript/changesu.sh

--- Shellscript/changesu.sh
+++ Shellscript/changesu.sh
@@ -2,6 +2,8 @@
 
 if [ "$1" == "brew" ]; then
     su bard -c "brew $@"
+elif [ "$1" == "mysql.server" ]; then
+    su bard -c "mysql.server $2"
 elif [ "$1" == "brewall" ]; then
     su bard -c "/Users/bard/Library/Scripts/brewall.sh"
 elif [ "$1" == "rtlogin" ]; then
