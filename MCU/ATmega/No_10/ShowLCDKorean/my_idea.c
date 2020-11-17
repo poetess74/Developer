81bfc536916798abc82834f0b4b758ebdde07533
hm <assa0620@gmail.com>
2020-11-17 12:07:23 +0900

    modified:   MCU/ATmega/No_10/ShowLCDKorean/my_idea.c 	modified:   MCU/ATmega/No_10/ShowLCDKorean/my_idea.cof 	modified:   MCU/ATmega/No_10/ShowLCDKorean/my_idea.lss 	modified:   MCU/ATmega/No_10/ShowLCDKorean/my_idea.ly1 	modified:   MCU/ATmega/No_10/ShowLCDKorean/my_idea.sym

--- MCU/ATmega/No_10/ShowLCDKorean/my_idea.c
+++ MCU/ATmega/No_10/ShowLCDKorean/my_idea.c
@@ -7,6 +7,8 @@
 #include <avr/io.h>              // Most basic include files
 #include <util/delay.h>
 
+#define ARRAY_SIZE 8
+
 void  LCD_command(unsigned char command) {
     PORTC = 0x04;    // E = 1, R/W=0, RS=0 (???)
     PORTA = command;
@@ -21,14 +23,6 @@ void LCD_data(unsigned char data) {
     PORTA = data;
     PORTC = 0x01;  // E = 0
     _delay_us(1);
-}
-
-void LCD_string(unsigned char command,  unsigned char *string) {
-    LCD_command(command);
-    while(*string != '\0') {
-        LCD_data(*string);
-        string++;
-    }
 }
 
 void LCD_display_ONOFF(int D, int C, int B) {
@@ -61,14 +55,14 @@ void LCD_initialize(void) {
 }
 
 int main(void) {
-    unsigned char data[] = {
-        0x09, 0x15, 0x15, 0x09, 0x00, 0x08, 0x0f, 0x00
+    unsigned char data[ARRAY_SIZE] = {
+        0x09, 0x15, 0x15, 0x09, 0x00, 0x08, 0x0F, 0x00
     };
     LCD_initialize();
     LCD_display_ONOFF(1, 0, 1);
     LCD_command(0x40);
 
-    for(int i = 0; i < 8; i++) {
+    for(int i = 0; i < ARRAY_SIZE; i++) {
         LCD_data(data[i]);
         _delay_ms(30);
     }
