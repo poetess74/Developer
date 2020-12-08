5ac6e5715fe892df1f6bf96769ffd14000370c16
hm <assa0620@gmail.com>
2020-12-08 16:18:54 +0900

    modified:   Shellscript/updater.sh

--- Shellscript/updater.sh
+++ Shellscript/updater.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-~/Documents/Release/brewall/brewall.sh
+if [ "$1" == "brew" ]; then
+    ~/Documents/Release/brewall/brewall.sh
+else
+    cd ~/Documents/Release/brewall
+    git pull origin master
+    cd - > /dev/null
+fi
 
 cd ~/Documents/Dev
 git pull origin master
