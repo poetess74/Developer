2377aca238783861717bd7a622b8799dd390534e
HyeongminKim <assa0620@gmail.com>
2020-12-09 20:55:41 +0900

    modified:   Shellscript/updater.sh

--- Shellscript/updater.sh
+++ Shellscript/updater.sh
@@ -4,16 +4,16 @@ if [ "$1" == "brew" ]; then
     ~/Documents/Release/brewall/brewall.sh
 else
     cd ~/Documents/Release/brewall
-    git pull origin master
+    git pull --rebase --stat origin master
     cd - > /dev/null
 fi
 
 cd ~/Documents/Dev
-git pull origin master
+git pull --rebase --stat origin master
 cd - > /dev/null
 
 cd ~/Documents/Release/Recruitment-Assistant
-git pull origin master
+git pull --rebase --stat origin master
 cd - > /dev/null
 
 softwareupdate -l
