a1b897e12cfcfef42998bcac5b2c3e63db8a822a
HyeongminKim <assa0620@gmail.com>
2020-12-19 12:41:01 +0900

    1 modified

--- Unity/Practice/World of warcraft/Assets/Scripts/UI/CreateCharacter.cs
+++ Unity/Practice/World of warcraft/Assets/Scripts/UI/CreateCharacter.cs
@@ -5,10 +5,6 @@ public class CreateCharacter : MonoBehaviour {
     [SerializeField] private InputField charName;
     [SerializeField] private Dropdown job;
 
-    private void Awake() {
-        charName = GetComponent<InputField>();
-    }
-
     private void OnClick() {
         charName.text = "";
         job.value = 0;
