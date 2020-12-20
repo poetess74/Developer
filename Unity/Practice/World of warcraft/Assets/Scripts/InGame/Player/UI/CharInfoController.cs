0aa4560fc6f1ee66302e63713eb178f6382dfd5a
HyeongminKim <assa0620@gmail.com>
2020-12-20 16:25:01 +0900

    4 modified

--- Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UI/CharInfoController.cs
+++ Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UI/CharInfoController.cs
@@ -8,7 +8,11 @@ public class CharInfoController : MonoBehaviour {
     [SerializeField] private Image userProfile;
     [SerializeField] private Image userJob;
     [SerializeField] private Slider userHP;
+    [SerializeField] private Text userHPText;
     [SerializeField] private Slider userSP;
+    [SerializeField] private Text userSPText;
+    [SerializeField] private Slider userEXP;
+    [SerializeField] private Text userEXPText;
     
     private void Start() {
         userName.text = GamePlayManager.PlayerName;
@@ -22,5 +26,7 @@ public class CharInfoController : MonoBehaviour {
         userLV.text = GamePlayManager.PlayerLV.ToString();
         userHP.value = GamePlayManager.PlayerCNTHP;
         userSP.value = GamePlayManager.PlayerCNTSP;
+        userHPText.text = GamePlayManager.PlayerCNTHP + " / " + GamePlayManager.PlayerHP;
+        userSPText.text = GamePlayManager.PlayerCNTSP + " / " + GamePlayManager.PlayerSP;
     }
 }
--- Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UI/MenuController.cs
+++ Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UI/MenuController.cs
@@ -2,10 +2,26 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour {
     [SerializeField] private GameObject menuBox;
-    private bool openMenu;
+    [SerializeField] private AudioClip menuOpen;
+    [SerializeField] private AudioClip menuClose;
+
+    private AudioSource audioSource;
+
+    private void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
     
     private void MenuOpen() {
-        openMenu = !openMenu;
-        menuBox.SetActive(openMenu);
+        audioSource.clip = menuBox.activeSelf ? menuClose : menuOpen;
+        audioSource.Play();
+        menuBox.SetActive(!menuBox.activeSelf);
+    }
+
+    private void Info() {
+        menuBox.SetActive(false);
+    }
+
+    private void Setting() {
+        menuBox.SetActive(false);
     }
 }
