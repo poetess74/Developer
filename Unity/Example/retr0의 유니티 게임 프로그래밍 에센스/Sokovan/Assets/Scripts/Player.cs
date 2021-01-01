a4674f122a76a1eee3eae20d7c60ea60f93aed3f
HyeongminKim <assa0620@gmail.com>
2021-01-01 22:29:04 +0900

    1 modified

--- Unity/Example/retr0의 유니티 게임 프로그래밍 에센스/Sokovan/Assets/Scripts/Player.cs
+++ Unity/Example/retr0의 유니티 게임 프로그래밍 에센스/Sokovan/Assets/Scripts/Player.cs
@@ -12,18 +12,10 @@ public class Player : MonoBehaviour {
     // Update is called once per frame (사양에 따라 가변적)
     // 영화 24fps, 모바일: 30fps, PC: 60fps
     private void Update() {
-        //유저 입력을 넣자
-        if(Input.GetKey(KeyCode.W)) {
-            playerRigidbody.AddForce(Vector3.forward * speed);
-        }
-        if(Input.GetKey(KeyCode.A)) {
-            playerRigidbody.AddForce(Vector3.left * speed);
-        }
-        if(Input.GetKey(KeyCode.S)) {
-            playerRigidbody.AddForce(Vector3.back * speed);
-        }
-        if(Input.GetKey(KeyCode.D)) {
-            playerRigidbody.AddForce(Vector3.right * speed);
-        }
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+        Vector3 velocity = new Vector3(inputX, 0, inputZ) * speed;
+        velocity.y = playerRigidbody.velocity.y;
+        playerRigidbody.velocity = velocity; 
     }
 }
