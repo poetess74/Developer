567ca7d5d01ba701aa181890eb19e50cbecc3b3b
HyeongminKim <assa0620@gmail.com>
2021-01-02 22:01:55 +0900

    2 modified

--- Unity/Example/retr0의 유니티 게임 프로그래밍 에센스/Sokovan/Assets/Scripts/GameManager.cs
+++ Unity/Example/retr0의 유니티 게임 프로그래밍 에센스/Sokovan/Assets/Scripts/GameManager.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
     public bool isGameOver;
     [SerializeField] private ItemBox[] itemBoxes;
+    [SerializeField] private GameObject winUI;
     
     // Start is called before the first frame update
     private void Start() {
         isGameOver = false;
+        winUI.SetActive(false);
     }
 
     // Update is called once per frame
     private void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            SceneManager.LoadScene(0);
+        }
+        
         if (isGameOver) return;
         
         int count = 0;
@@ -20,7 +27,7 @@ public class GameManager : MonoBehaviour {
 
         if(count == itemBoxes.Length) {
             isGameOver = true;
-            Debug.Log("Player Win!");
+            winUI.SetActive(true);
         }
     }
 }
