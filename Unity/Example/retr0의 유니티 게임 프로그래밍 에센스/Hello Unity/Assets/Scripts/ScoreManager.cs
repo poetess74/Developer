42d85660bf1689b7f1b18c3431ee122457023f36
HyeongminKim <assa0620@gmail.com>
2021-01-05 14:46:33 +0900

    6 added

---
+++ Unity/Example/retr0의 유니티 게임 프로그래밍 에센스/Hello Unity/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+    [SerializeField] private List<int> scores = new List<int>();
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            if(scores.Count > 0) return;
+            for(int i = 0; i < 16; i++) {
+                scores.Add((int)Mathf.Pow(2, i));
+            }
+        } else if(Input.GetMouseButtonDown(1)) {
+            scores.Clear();
+        }
+    }
+}
