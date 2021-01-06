647a7ad061c0a241c0ed91044a3d7532ed7df7de
HyeongminKim <assa0620@gmail.com>
2021-01-06 16:31:36 +0900

    4 added 5 modified

--- Unity/Example/retr0의 유니티 게임 프로그래밍 에센스/Amazing Bowling/Assets/Scripts/Ball.cs
+++ Unity/Example/retr0의 유니티 게임 프로그래밍 에센스/Amazing Bowling/Assets/Scripts/Ball.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
+    [SerializeField] private LayerMask filterProp;
+    
     [SerializeField] private float maxDamage = 100f;
     [SerializeField] private float explosionForce = 1000f;
     [SerializeField] private float explosionRadius = 20f;
@@ -17,11 +19,31 @@ public class Ball : MonoBehaviour {
     }
 
     private void OnTriggerEnter(Collider other) {
+        var colliders = Physics.OverlapSphere(transform.position, explosionRadius, filterProp);
+
+        foreach(var filteredCollider in colliders) {
+            Rigidbody targetRigidbody = filteredCollider.GetComponent<Rigidbody>();
+            targetRigidbody.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+
+            Prop targetProp = filteredCollider.GetComponent<Prop>();
+
+            float damage = CalcDamage(filteredCollider.transform.position);
+            targetProp.TakeDamage(damage);
+        }
+        
         explosionParticle.transform.parent = null;
         explosionParticle.Play();
         explosionAudio.Play();
         
         Destroy(explosionParticle.gameObject, explosionParticle.main.duration);
         Destroy(gameObject);
+    }
+
+    private float CalcDamage(Vector3 targetPos) {
+        Vector3 explosionToTarget = targetPos - transform.position;
+        float distance = explosionToTarget.magnitude;
+        float edgeToCenterDistance = explosionRadius - distance;
+        float ratio = edgeToCenterDistance / explosionRadius;
+        return Mathf.Max(maxDamage * ratio, 0);
     }
 }
