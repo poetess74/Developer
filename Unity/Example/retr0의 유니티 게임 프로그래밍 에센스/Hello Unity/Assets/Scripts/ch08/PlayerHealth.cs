82ba4d87674cef472c4188ca12d7a686e537c425
HyeongminKim <assa0620@gmail.com>
2021-01-12 22:04:07 +0900

    2 modified

--- Unity/Example/retr0의 유니티 게임 프로그래밍 에센스/Hello Unity/Assets/Scripts/ch08/PlayerHealth.cs
+++ Unity/Example/retr0의 유니티 게임 프로그래밍 에센스/Hello Unity/Assets/Scripts/ch08/PlayerHealth.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerHealth : MonoBehaviour {
+    [SerializeField] private UnityEvent onPlayerDead;
+    
     private void Dead() {
+        onPlayerDead.Invoke();
         Debug.Log("죽었다!");
         Destroy(gameObject);
     }
