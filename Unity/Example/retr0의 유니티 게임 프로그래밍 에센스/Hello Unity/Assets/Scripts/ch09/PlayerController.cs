eb76c76dc27c5e3c1d75fd9f7573b654f08afa26
HyeongminKim <assa0620@gmail.com>
2021-01-14 17:13:24 +0900

    3 modified

--- Unity/Example/retr0의 유니티 게임 프로그래밍 에센스/Hello Unity/Assets/Scripts/ch09/PlayerController.cs
+++ Unity/Example/retr0의 유니티 게임 프로그래밍 에센스/Hello Unity/Assets/Scripts/ch09/PlayerController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private Animator anim;
+    [SerializeField] private Transform target;
+    
     // Start is called before the first frame update
     private void Start() {
         anim = GetComponent<Animator>();
@@ -17,5 +19,16 @@ public class PlayerController : MonoBehaviour {
         float horizontalInput = Input.GetAxis("Horizontal");
         anim.SetFloat("Speed", verticalInput);
         anim.SetFloat("Direction", horizontalInput);
+    }
+
+    private void OnAnimatorIK(int layerIndex) {
+        anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, 0.5f);
+        anim.SetIKRotationWeight(AvatarIKGoal.LeftHand, 0.5f);
+        
+        anim.SetIKPosition(AvatarIKGoal.LeftHand, target.position);
+        anim.SetIKRotation(AvatarIKGoal.LeftHand, target.rotation);
+        
+        anim.SetLookAtWeight(1.0f);
+        anim.SetLookAtPosition(target.position);
     }
 }
