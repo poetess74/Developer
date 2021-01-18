16e8a0f0f7420e3d104c4da3c618699e15301b75
HyeongminKim <assa0620@gmail.com>
2021-01-18 20:16:14 +0900

    2 added 1 modified

--- Unity/Example/retr0의 유니티 게임 프로그래밍 에센스/Hello Unity/Assets/Scripts/ch10/ColorChange.cs
+++ Unity/Example/retr0의 유니티 게임 프로그래밍 에센스/Hello Unity/Assets/Scripts/ch10/ColorChange.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ColorChange : MonoBehaviour {
+    [SerializeField] private Toggle toggle;
+
+    private void Start() {
+        Debug.Log(toggle.isOn);
+    }
+    
     public void ChangeColor() {
         GetComponent<Renderer>().material.color = Random.ColorHSV();
+    }
+    
+    public void ChangeColor(bool isOn) {
+        GetComponent<Renderer>().material.color = isOn ? Random.ColorHSV() : Color.white;
     }
 }
