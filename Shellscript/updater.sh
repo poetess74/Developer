b461938dd4059acc1569f7606db49e5a9d07b7ec
HyeongminKim <assa0620@gmail.com>
2021-01-21 19:34:01 +0900

    modified:   Shellscript/updater.sh

--- Shellscript/updater.sh
+++ Shellscript/updater.sh
@@ -2,8 +2,15 @@
 
 if [ "$1" == "brew" ]; then
     ~/Documents/Release/brewall/brewall.sh
+elif [ "$1" == "apt" ]; then
+    ~/Documents/Release/aptall/aptall.sh
 else
     cd ~/Documents/Release/brewall 2> /dev/null
+    if [ $? == 0 ]; then
+        git pull --rebase --stat origin master
+        cd - > /dev/null
+    fi
+    cd ~/Documents/Release/aptall 2> /dev/null
     if [ $? == 0 ]; then
         git pull --rebase --stat origin master
         cd - > /dev/null
