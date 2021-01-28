2b031eba78f0f84c4527be275c7bc2e6681195cd
HyeongminKim <assa0620@gmail.com>
2021-01-28 16:19:26 +0900

    2 modified

--- Unity/Example/retr0의 유니티 게임 프로그래밍 에센스/TPS Start Project/Assets/Scripts/ItemSpawner.cs
+++ Unity/Example/retr0의 유니티 게임 프로그래밍 에센스/TPS Start Project/Assets/Scripts/ItemSpawner.cs
@@ -1,23 +1,36 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class ItemSpawner : MonoBehaviour {
-    public GameObject[] items;
-    public Transform playerTransform;
-    public float maxDistance = 5f;
+    [SerializeField] private GameObject[] items;
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float maxDistance = 5f;
 
-    public float timeBetSpawnMax = 7f;
-    public float timeBetSpawnMin = 2f;
+    [SerializeField] private float timeBetSpawnMax = 7f;
+    [SerializeField] private float timeBetSpawnMin = 2f;
 
     private float lastSpawnTime;
 
     private float timeBetSpawn;
 
     private void Start() {
+        timeBetSpawn = Random.Range(timeBetSpawnMin, timeBetSpawnMax);
+        lastSpawnTime = 0f;
     }
 
     private void Update() {
+        if(!(Time.time >= lastSpawnTime + timeBetSpawn) || playerTransform == null) return;
+        Spawn();
+        lastSpawnTime = Time.time;
+        timeBetSpawn = Random.Range(timeBetSpawnMin, timeBetSpawnMax);
     }
 
     private void Spawn() {
+        Vector3 spawnPosition = Utility.GetRandomPointOnNavMesh(
+            playerTransform.position, maxDistance, NavMesh.AllAreas
+        );
+        spawnPosition += Vector3.up * 0.5f;
+        GameObject item = Instantiate(items[Random.Range(0, items.Length)], spawnPosition, Quaternion.identity);
+        Destroy(item, 5f);
     }
 }
