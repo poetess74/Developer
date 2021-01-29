cd0e7a8bff51baef489d3494a2e800c1d5a8afa4
HyeongminKim <assa0620@gmail.com>
2021-01-29 12:57:38 +0900

    modified:   Unity/Practice/World of warcraft/Assets/Prefabs/InGame/Object/Player.prefab 	modified:   Unity/Practice/World of warcraft/Assets/Prefabs/InGame/UI/HUD.prefab 	modified:   Unity/Practice/World of warcraft/Assets/Prefabs/InGame/UI/SkillButton.prefab 	modified:   Unity/Practice/World of warcraft/Assets/Prefabs/Intro/Canvas.prefab 	modified:   Unity/Practice/World of warcraft/Assets/Prefabs/Intro/PlayableCharacter.prefab 	modified:   Unity/Practice/World of warcraft/Assets/Scenes/InGame/Field.unity 	modified:   Unity/Practice/World of warcraft/Assets/Scenes/UI/Intro.unity 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/DataBase/GamePlayManager.cs 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/DataBase/GamePlayManager.cs.meta 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/InitialPlayerController.cs 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/InitialPlayerController.cs.meta 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/PlayerController.cs 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/PlayerController.cs.meta 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/SkillTriggerController.cs 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/SkillTriggerController.cs.meta 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UI/CharInfoController.cs 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UI/CharInfoController.cs.meta 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UI/MenuController.cs 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UI/MenuController.cs.meta 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UI/ProgressController.cs 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UI/ProgressController.cs.meta 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UI/SkillTreeController.cs 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UI/SkillTreeController.cs.meta 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UI/TargetingController.cs 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UI/TargetingController.cs.meta 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UI/WarningController.cs 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UI/WarningController.cs.meta 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UserController.cs 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/InGame/Player/UserController.cs.meta 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/UI.meta 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/UI/AvatarRotator.cs 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/UI/AvatarRotator.cs.meta 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/UI/CharacterDetailController.cs 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/UI/CharacterDetailController.cs.meta 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/UI/CreateCharacter.cs 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/UI/CreateCharacter.cs.meta 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/UI/GameController.cs 	deleted:    Unity/Practice/World of warcraft/Assets/Scripts/UI/GameController.cs.meta 	modified:   Unity/Practice/World of warcraft/UserSettings/EditorUserSettings.asset

--- Unity/Practice/World of warcraft/Assets/Scripts/UI/AvatarRotator.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class AvatarRotator : MonoBehaviour {
-    private float rotationSpeed = 50f;
-    private void Update() {
-        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
-    }
-}
