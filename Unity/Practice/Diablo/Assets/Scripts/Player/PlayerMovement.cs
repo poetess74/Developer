433e4d855436daf9a57b8ffab258fcbd10c231cf
HyeongminKim <assa0620@gmail.com>
2021-02-15 13:17:41 +0900

    1 modified

--- Unity/Practice/Diablo/Assets/Scripts/Player/PlayerMovement.cs
+++ Unity/Practice/Diablo/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,8 @@ namespace Player {
     public class PlayerMovement : MonoBehaviour {
         private float speed = 3f;
         private float idlingTime;
+        private float movingTime;
+        private Vector3 moveDir;
         
         private PlayerInput playerInput;
         private Animator animator;
@@ -48,15 +50,26 @@ namespace Player {
         }
 
         private void Move() {
-            float distance = Vector3.Distance(transform.position, playerInput.moveDir);
+            float distance = movingTime > 9f ? 0f : Vector3.Distance(transform.position, playerInput.moveDir);
+                
             if(distance >= 0.1f) {
+                if(moveDir == playerInput.moveDir) {
+                    movingTime += Time.deltaTime;
+                } else {
+                    movingTime = 0f;
+                    moveDir = playerInput.moveDir;
+                }
+                
                 animator.SetFloat("Movement", distance, 0.05f, Time.deltaTime);
                 
                 Vector3 direction = playerInput.moveDir - transform.position;
                 direction = Vector3.Normalize(direction);
                 characterController.Move(direction * Time.deltaTime * speed);
             } else {
-                animator.SetFloat("Movement", 0);
+                animator.SetFloat("Movement", 0f);
+                if(moveDir != playerInput.moveDir) {
+                    movingTime = 0f;
+                }
             }
         }
 
