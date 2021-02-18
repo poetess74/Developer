92afa002cf3a97fccfd3e85a78be329e301d06aa
HyeongminKim <assa0620@gmail.com>
2021-02-18 14:32:54 +0900

    2 modified

--- Unity/Practice/Diablo/Assets/Scripts/Player/PlayerAttack.cs
+++ Unity/Practice/Diablo/Assets/Scripts/Player/PlayerAttack.cs
@@ -15,7 +15,7 @@ namespace Player {
             if(input.target == null || GamePlayManager.instance.isGameOver) return;
             
             var targetHealth = input.target.GetComponent<IDamageable>();
-            if(Input.GetKeyDown(KeyCode.Alpha1)) {
+            if(Input.GetButtonDown("Fire1")) {
                 if(input.target.GetComponent<EnemyDamage>().enemyCNTHP <= 0f) return;
                 
                 targetHealth.Damaged(status.strength, false, gameObject);
