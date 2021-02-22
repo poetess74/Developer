4786bc369ed983b834bc741a6c52d16c9cffb193
HyeongminKim <assa0620@gmail.com>
2021-02-22 20:13:22 +0900

    2 modified

--- Unity/Practice/Diablo/Assets/Scripts/Player/UI/PlayerInspector.cs
+++ Unity/Practice/Diablo/Assets/Scripts/Player/UI/PlayerInspector.cs
@@ -3,18 +3,27 @@ using UnityEngine;
 namespace Player.UI {
     public class PlayerInspector : MonoBehaviour {
         
+        private GameObject menuWindow;
         private GameObject itemWindow;
         private GameObject skillWindow;
         private GameObject statusWindow;
 
         private void Start() {
-            itemWindow = transform.GetChild(0).gameObject;
-            skillWindow = transform.GetChild(1).gameObject;
-            statusWindow = transform.GetChild(2).gameObject;
+            menuWindow = transform.GetChild(0).gameObject;
+            itemWindow = transform.GetChild(1).gameObject;
+            skillWindow = transform.GetChild(2).gameObject;
+            statusWindow = transform.GetChild(3).gameObject;
         }
 
         private void Update() {
+            if(Input.GetKeyDown(KeyCode.Escape)) {
+                if(!menuWindow.activeSelf && GamePlayManager.instance.interrupt) return;
+                menuWindow.SetActive(!menuWindow.activeSelf);
+                GamePlayManager.instance.interrupt = !GamePlayManager.instance.interrupt;
+            }
+
             if(GamePlayManager.instance.isGameOver) return;
+            
             if(Input.GetKeyDown(KeyCode.I)) {
                 if(!itemWindow.activeSelf && GamePlayManager.instance.interrupt) return;
                 itemWindow.SetActive(!itemWindow.activeSelf);
