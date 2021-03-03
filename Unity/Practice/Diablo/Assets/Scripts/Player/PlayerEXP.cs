c9371ba1a11704c3dc2f33fc1f37ad2ccf4910e5
HyeongminKim <assa0620@gmail.com>
2021-03-04 01:01:35 +0900

    1 modified

--- Unity/Practice/Diablo/Assets/Scripts/Player/PlayerEXP.cs
+++ Unity/Practice/Diablo/Assets/Scripts/Player/PlayerEXP.cs
@@ -12,17 +12,29 @@ namespace Player {
             expMax = 400;
         }
 
-        public void LevelUp() {
+        private void LevelUp() {
             status.healthPointCNT = status.healthPoint;
             status.manaPointCNT = status.manaPoint;
             status.skillPoint += 3;
             GamePlayManager.instance.stageLV++;
         }
 
+        private bool LevelCap() {
+            if(GamePlayManager.instance.stageLV >= 70) {
+                status.playerEXP = 0f;
+                expMax = 0f;
+                return true;
+            }
+            return false;
+        }
+
         public void AddPlayerEXP(float exp) {
+            if(LevelCap()) return;
+            
             status.playerEXP += exp;
             while(status.playerEXP > expMax) {
                 LevelUp();
+                if(LevelCap()) return;
                 status.playerEXP -= expMax;
                 expMax *= 2;
             }
