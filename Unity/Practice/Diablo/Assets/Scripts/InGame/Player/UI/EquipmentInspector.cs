8e0ee34cd4faa6257c2cd42aeea4d4f80dd0fefe
HyeongminKim <assa0620@gmail.com>
2021-03-10 20:12:02 +0900

    2 modified

--- Unity/Practice/Diablo/Assets/Scripts/InGame/Player/UI/EquipmentInspector.cs
+++ Unity/Practice/Diablo/Assets/Scripts/InGame/Player/UI/EquipmentInspector.cs
@@ -2,14 +2,23 @@ using UnityEngine;
 
 namespace InGame.Player.UI {
     public class EquipmentInspector : MonoBehaviour {
-        // Start is called before the first frame update
-        private void Start() {
-            
+        private GameObject[] detail;
+        private GameObject[] location;
+
+        private void OnEnable() {
+            if(detail != null && location != null) return;
+            detail = new GameObject[transform.GetChild(3).transform.childCount];
+            for(int i = 0; i < detail.Length; i++) {
+                detail[i] = transform.GetChild(3).transform.GetChild(i).gameObject;
+            }
+            location = new GameObject[transform.GetChild(4).transform.childCount];
+            for(int i = 0; i < location.Length; i++) {
+                location[i] = transform.GetChild(4).transform.GetChild(i).gameObject;
+            }
         }
 
-        // Update is called once per frame
         private void Update() {
-
+            //TODO: Update detail status in equipment and apply equipment in dropbox.
         }
     }
 }
