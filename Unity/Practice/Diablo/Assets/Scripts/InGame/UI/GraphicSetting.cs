99fbf5aaffeb546364eb03eae5924fd6f72fe8c9
HyeongminKim <assa0620@gmail.com>
2021-03-11 13:27:10 +0900

    5 added 1 modified

---
+++ Unity/Practice/Diablo/Assets/Scripts/InGame/UI/GraphicSetting.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace InGame.UI {
+    public class GraphicSetting : MonoBehaviour {
+        private void Start() {
+
+        }
+
+        private void Update() {
+
+        }
+    }
+}
