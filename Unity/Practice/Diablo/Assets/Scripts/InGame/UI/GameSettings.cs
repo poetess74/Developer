471d0d848d06cc0b7410bd4c70160a7af682fe94
HyeongminKim <assa0620@gmail.com>
2021-03-16 21:19:40 +0900

    2 modified

--- Unity/Practice/Diablo/Assets/Scripts/InGame/UI/GameSettings.cs
+++ Unity/Practice/Diablo/Assets/Scripts/InGame/UI/GameSettings.cs
@@ -1,13 +1,19 @@
+using InGame.Player;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace InGame.UI {
     public class GameSettings : MonoBehaviour {
-        private void Start() {
+        private MapCamMovement map;
 
+        private Toggle mapRotation;
+        private void OnEnable() {
+            map = FindObjectOfType<MapCamMovement>();
+            mapRotation = transform.GetChild(2).gameObject.GetComponent<Toggle>();
         }
 
         private void Update() {
-
+            map.constRotate = !mapRotation.isOn;
         }
     }
 }
