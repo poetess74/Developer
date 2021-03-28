b7e87c6916bcc779fc6f5fea350b208ea534357c
HyeongminKim <assa0620@gmail.com>
2021-03-28 16:15:46 +0900

    modified:   Shellscript/updater.sh

--- Shellscript/updater.sh
+++ Shellscript/updater.sh
@@ -1,42 +1,31 @@
 #!/bin/bash
-cntBranch=$(git branch | sed '/* /!d'| sed 's/* //g')
 
 if [ "$1" == "brew" ]; then
     ~/Documents/Release/brewall/brewall.sh
-    cd ~/Documents/Release/aptall 2> /dev/null
-    if [ $? == 0 ]; then
-        git pull --rebase --stat origin $cntBranch
-        cd - > /dev/null
-    fi
 elif [ "$1" == "apt" ]; then
     ~/Documents/Release/aptall/aptall.sh
-    cd ~/Documents/Release/brewall 2> /dev/null
-    if [ $? == 0 ]; then
-        git pull --rebase --stat origin $cntBranch
-        cd - > /dev/null
-    fi
 else
     cd ~/Documents/Release/brewall 2> /dev/null
     if [ $? == 0 ]; then
-        git pull --rebase --stat origin $cntBranch
+        git pull --rebase --stat origin $(git branch | sed '/* /!d'| sed 's/* //g')
         cd - > /dev/null
     fi
     cd ~/Documents/Release/aptall 2> /dev/null
     if [ $? == 0 ]; then
-        git pull --rebase --stat origin $cntBranch
+        git pull --rebase --stat origin $(git branch | sed '/* /!d'| sed 's/* //g')
+        cd - > /dev/null
+    fi
+    cd ~/Documents/Dev 2> /dev/null
+    if [ $? == 0 ]; then
+        git pull --rebase --stat origin $(git branch | sed '/* /!d'| sed 's/* //g')
+        cd - > /dev/null
+    fi
+    cd ~/Documents/Release/Recruitment-Assistant 2> /dev/null
+    if [ $? == 0 ]; then
+        git pull --rebase --stat origin $(git branch | sed '/* /!d'| sed 's/* //g')
         cd - > /dev/null
     fi
 fi
 
-cd ~/Documents/Dev 2> /dev/null
-if [ $? == 0 ]; then
-    git pull --rebase --stat origin $cntBranch
-    cd - > /dev/null
-fi
 
-cd ~/Documents/Release/Recruitment-Assistant 2> /dev/null
-if [ $? == 0 ]; then
-    git pull --rebase --stat origin $cntBranch
-    cd - > /dev/null
-fi
 
