9b46f9080206dd51a819c4875656f109e3d36180
HyeongminKim <assa0620@gmail.com>
2021-03-30 23:20:01 +0900

    modified:   Shellscript/updater.sh

--- Shellscript/updater.sh
+++ Shellscript/updater.sh
@@ -2,27 +2,6 @@
 
 exitCode=0
 
-if [ "$(uname -s)" == "Darwin" ]; then
-    if [ -x ~/Documents/Release/brewall/brewall.sh ]; then
-        ~/Documents/Release/brewall/brewall.sh $@
-        exitCode=$?
-    else
-        gitUpdate
-    fi
-elif [ "$(uname -s)" == "Linux" ]; then
-    if [ -x ~/Documents/Release/aptall/aptall.sh ]; then
-        ~/Documents/Release/aptall/aptall.sh $@
-        exitCode=$?
-    else
-        gitUpdate
-    fi
-else
-    gitUpdate
-fi
-
-echo -e '\a'
-exit $exitCode
-
 function gitUpdate() {
     cd ~/Documents/Release/brewall 2> /dev/null
     if [ $? == 0 ]; then
@@ -46,5 +25,30 @@ function gitUpdate() {
     fi
 }
 
+if [ "$SKIP_PACKAGE_UPDATER" == "true" ]; then
+    gitUpdate
+    echo -e '\a'
+    exit $exitCode
+fi
 
+if [ "$(uname -s)" == "Darwin" ]; then
+    if [ -x ~/Documents/Release/brewall/brewall.sh ]; then
+        ~/Documents/Release/brewall/brewall.sh $@
+        exitCode=$?
+    else
+        gitUpdate
+    fi
+elif [ "$(uname -s)" == "Linux" ]; then
+    if [ -x ~/Documents/Release/aptall/aptall.sh ]; then
+        ~/Documents/Release/aptall/aptall.sh $@
+        exitCode=$?
+    else
+        gitUpdate
+    fi
+else
+    gitUpdate
+fi
+
+echo -e '\a'
+exit $exitCode
 
