7e8c34064d1a1da5d4f880d64be313190e8b30f3
HyeongminKim <assa0620@gmail.com>
2021-04-06 14:52:44 +0900

    2 modified

--- AndroidStudioProjects/Example/Android Studio를 활용한 안드로이드 프로그래밍(6판)/ImageViewer/app/src/main/java/com/example/imageviewer/MainActivity.java
+++ AndroidStudioProjects/Example/Android Studio를 활용한 안드로이드 프로그래밍(6판)/ImageViewer/app/src/main/java/com/example/imageviewer/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.imageviewer;
 
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView view;
+    ImageButton button;
+    int angle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        view = findViewById(R.id.imageView);
+        button = findViewById(R.id.imageBtn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                angle += 30;
+                if(angle == 360) angle = 0;
+                view.setRotation(angle);
+            }
+        });
     }
 }
