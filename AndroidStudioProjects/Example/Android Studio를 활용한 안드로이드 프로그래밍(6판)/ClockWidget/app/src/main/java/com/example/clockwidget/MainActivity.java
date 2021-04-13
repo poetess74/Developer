55352a818a695ed1e90762fa991423ff31a91005
HyeongminKim <assa0620@gmail.com>
2021-04-13 14:40:42 +0900

    33 added

---
+++ AndroidStudioProjects/Example/Android Studio를 활용한 안드로이드 프로그래밍(6판)/ClockWidget/app/src/main/java/com/example/clockwidget/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.clockwidget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView dateDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("시계 위젯");
+        dateDisplay = findViewById(R.id.date);
+        dateDisplay.setText(this.getDate());
+    }
+
+    private String getDate() {
+        return DateFormat.getDateInstance().format(new Date());
+    }
+}
