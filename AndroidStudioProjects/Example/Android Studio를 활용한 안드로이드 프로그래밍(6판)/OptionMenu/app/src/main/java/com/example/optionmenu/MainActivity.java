959390411fed948559163ddbede3121eff7bfbdb
HyeongminKim <assa0620@gmail.com>
2021-04-20 16:52:46 +0900

    34 added

---
+++ AndroidStudioProjects/Example/Android Studio를 활용한 안드로이드 프로그래밍(6판)/OptionMenu/app/src/main/java/com/example/optionmenu/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.optionmenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.SubMenu;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    LinearLayout baseLayout;
+    Button button1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("배경색 바꾸기");
+        baseLayout = findViewById(R.id.baseLayout);
+        button1 = findViewById(R.id.button1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mInflater = getMenuInflater();
+        mInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemRed:
+                baseLayout.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.itemGreen:
+                baseLayout.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.itemBlue:
+                baseLayout.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.subRotate:
+                button1.setRotation(45);
+                return true;
+            case R.id.subSize:
+                button1.setScaleX(2);
+                return true;
+        }
+        return false;
+    }
+}
