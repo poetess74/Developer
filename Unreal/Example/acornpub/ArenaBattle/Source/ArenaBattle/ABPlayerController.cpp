ca40ccd80d2ea12abb2c06b97a076489280d1243
HyeongminKim <assa0620@gmail.com>
2021-06-20 19:51:42 +0900

    7 added 1 modified

---
+++ Unreal/Example/acornpub/ArenaBattle/Source/ArenaBattle/ABPlayerController.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABPlayerController.h"
+
+void AABPlayerController::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	ABLOG_S(Warning);
+}
+
+void AABPlayerController::OnPossess(APawn* InPawn)
+{
+	ABLOG_S(Warning);
+	Super::OnPossess(InPawn);
+}
+
