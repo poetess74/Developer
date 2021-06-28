2819975296082bd2d43aae57ff2aa5d42e1811d1
HyeongminKim <assa060@gmail.com>
2021-06-28 15:11:59 +0900

    1 added 2 modified

--- Unreal/Example/acornpub/ArenaBattle/Source/ArenaBattle/ABPawn.cpp
+++ Unreal/Example/acornpub/ArenaBattle/Source/ArenaBattle/ABPawn.cpp
@@ -26,6 +26,14 @@ AABPawn::AABPawn()
 	SpringArm->TargetArmLength = 400.0f;
 	SpringArm->SetRelativeRotation(FRotator(-15.0f, 0.0f, 0.0f));
 
+	Mesh->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+
+	static ConstructorHelpers::FClassFinder<UAnimInstance> WARRIOR_ANIM(TEXT("/Game/Book/Animations/WarriorAnimBlueprint.WarriorAnimBlueprint_C"));
+	if(WARRIOR_ANIM.Succeeded())
+	{
+		Mesh->SetAnimInstanceClass(WARRIOR_ANIM.Class);
+	}
+
 	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SK_CARDBOARD(TEXT("/Game/InfinityBladeWarriors/Character/CompleteCharacters/SK_CharM_Cardboard.SK_CharM_Cardboard"));
     if (SK_CARDBOARD.Succeeded())
     {
