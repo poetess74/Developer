eddcedf9d392f8d489331781801fc5f5d46525a0
HyeongminKim <assa060@gmail.com>
2021-07-13 15:40:06 +0900

    1 modified

--- Unity/Practice/PracticeCalc/Assets/Scripts/Test/QuestionStatus.cs
+++ Unity/Practice/PracticeCalc/Assets/Scripts/Test/QuestionStatus.cs
@@ -1,45 +1,47 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class QuestionStatus : MonoBehaviour {
-    [SerializeField] private Text questionCount;
-    [SerializeField] private Text performance;
+namespace Test {
+    public class QuestionStatus : MonoBehaviour {
+        [SerializeField] private Text questionCount;
+        [SerializeField] private Text performance;
 
-    [HideInInspector] public bool maxAnswerCountReached;
-    [HideInInspector] public int currentAnswerCount = 1;
+        [HideInInspector] public bool maxAnswerCountReached;
+        [HideInInspector] public int currentAnswerCount = 1;
 
-    private int providedAnswerCount;
-    private int correctAnswerCount;
+        private int providedAnswerCount;
+        private int correctAnswerCount;
 
-    public void SetQuestionCount(int maxAnswerCount, bool init) {
-        if(init) {
-            correctAnswerCount = maxAnswerCount;
+        public void SetQuestionCount(int maxAnswerCount, bool init) {
+            if(init) {
+                correctAnswerCount = maxAnswerCount;
 
-            providedAnswerCount = maxAnswerCount;
-            questionCount.text = $"{maxAnswerCount}문항 중 {currentAnswerCount}번";
+                providedAnswerCount = maxAnswerCount;
+                questionCount.text = $"{maxAnswerCount}문항 중 {currentAnswerCount}번";
 
-            return;
+                return;
+            }
+
+            if(providedAnswerCount > currentAnswerCount) {
+                currentAnswerCount++;
+
+                questionCount.text = $"{maxAnswerCount}문항 중 {currentAnswerCount}번";
+            } else {
+                maxAnswerCountReached = true;
+            }
         }
 
-        if(providedAnswerCount > currentAnswerCount) {
-            currentAnswerCount++;
+        public void SetPerformance(bool correctAnswer) {
+            if(!correctAnswer) {
+                correctAnswerCount--;
+            }
 
-            questionCount.text = $"{maxAnswerCount}문항 중 {currentAnswerCount}번";
-        } else {
-            maxAnswerCountReached = true;
-        }
-    }
-
-    public void SetPerformance(bool correctAnswer) {
-        if(!correctAnswer) {
-            correctAnswerCount--;
+            float percent = (float) correctAnswerCount / providedAnswerCount * 100;
+            performance.text = $"정확도: {percent:F2}%";
         }
 
-        float percent = (float) correctAnswerCount / providedAnswerCount * 100;
-        performance.text = $"정확도: {percent:F2}%";
-    }
-
-    public void SetPerformance() {
-        performance.text = "정확도: 100.00%";
+        public void SetPerformance() {
+            performance.text = "정확도: 100.00%";
+        }
     }
 }
