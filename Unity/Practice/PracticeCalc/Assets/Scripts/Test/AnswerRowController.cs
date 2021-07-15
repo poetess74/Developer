43c7517e640b82bddc8c3eff5670e1d56c1f8343
HyeongminKim <assa060@gmail.com>
2021-07-15 12:40:12 +0900

    1 modified

--- Unity/Practice/PracticeCalc/Assets/Scripts/Test/AnswerRowController.cs
+++ Unity/Practice/PracticeCalc/Assets/Scripts/Test/AnswerRowController.cs
@@ -1,20 +1,22 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AnswerRowController : MonoBehaviour {
-    [SerializeField] private Text listID;
-    [SerializeField] private Text question;
-    [SerializeField] private Text answer;
-    [SerializeField] private Image status;
+namespace Test {
+    public class AnswerRowController : MonoBehaviour {
+        [SerializeField] private Text listID;
+        [SerializeField] private Text question;
+        [SerializeField] private Text answer;
+        [SerializeField] private Image status;
 
-    public string ValueMerger(int firstNum, string op, int secondNum) {
-        return firstNum + " " + op + " " + secondNum;
-    }
+        public string ValueMerger(int firstNum, string op, int secondNum) {
+            return firstNum + " " + op + " " + secondNum;
+        }
 
-    public void SetRowElement(int id, string question, string answer, bool successful) {
-        listID.text = id.ToString();
-        this.question.text = question;
-        this.answer.text = answer;
-        status.color = successful ? Color.blue : Color.red;
+        public void SetRowElement(int id, string question, string answer, bool successful) {
+            listID.text = id.ToString();
+            this.question.text = question;
+            this.answer.text = answer;
+            status.color = successful ? Color.blue : Color.red;
+        }
     }
 }
