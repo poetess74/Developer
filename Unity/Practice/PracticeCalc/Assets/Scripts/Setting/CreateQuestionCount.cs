3e39ea700f746db42b2bda36d5a8a2ee1e97498f
HyeongminKim <assa060@gmail.com>
2021-07-16 13:11:42 +0900

    2 modified

--- Unity/Practice/PracticeCalc/Assets/Scripts/Setting/CreateQuestionCount.cs
+++ Unity/Practice/PracticeCalc/Assets/Scripts/Setting/CreateQuestionCount.cs
@@ -5,18 +5,23 @@ namespace Setting {
     public class CreateQuestionCount : MonoBehaviour {
         [HideInInspector] public Toggle random;
         [HideInInspector] public InputField count;
+        [HideInInspector] public Text info;
 
         private void Start() {
             random = transform.GetChild(1).GetComponent<Toggle>();
             count = transform.GetChild(2).GetComponent<InputField>();
+            info = transform.GetChild(3).GetComponent<Text>();
+
             count.text = GameManager.instance.providedQuestionCount.ToString();
         }
 
         private void Update() {
             if(random.isOn) {
                 count.gameObject.SetActive(false);
+                info.gameObject.SetActive(true);
             } else {
                 count.gameObject.SetActive(true);
+                info.gameObject.SetActive(false);
 
                 if(count.text.Length == 0) return;
                 GameManager.instance.providedQuestionCount = int.Parse(count.text);
