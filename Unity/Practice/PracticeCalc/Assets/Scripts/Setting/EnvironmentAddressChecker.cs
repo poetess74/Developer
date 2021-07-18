6ba7a62d9f9c10dbf25fb11ec6e7f628eaea5684
HyeongminKim <assa060@gmail.com>
2021-07-18 23:07:26 +0900

    2 modified

--- Unity/Practice/PracticeCalc/Assets/Scripts/Setting/EnvironmentAddressChecker.cs
+++ Unity/Practice/PracticeCalc/Assets/Scripts/Setting/EnvironmentAddressChecker.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 namespace Setting {
     public class EnvironmentAddressChecker : MonoBehaviour {
         private GameObject clientInput;
+        private GameObject connectedAddress;
         private void Start() {
             clientInput = transform.GetChild(0).gameObject;
+            connectedAddress = transform.GetChild(1).gameObject;
         }
 
         private void Update() {
