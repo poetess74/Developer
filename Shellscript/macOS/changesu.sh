c93e0940eba915ce032c100fb070b00c8a81e669
HyeongminKim <assa0620@gmail.com>
2021-07-24 17:13:59 +0900

    modified:   Shellscript/macOS/changesu.sh

--- Shellscript/macOS/changesu.sh
+++ Shellscript/macOS/changesu.sh
@@ -10,6 +10,10 @@ elif [ "$1" == "brewall" ]; then
     su bard -c "~/Library/Scripts/brewall/brewall.sh;"
     ln -sF $(find /usr/local/Cellar/macvim/ -name "MacVim.app") ~/Applications/  
     result=$?
+
+    echo "Enter password for poetess. "
+    su poetess -c "source ~/.zshrc && omz update"
+    result=$?
 elif [ "$1" == "rtlogin" ]; then
     su bard -c "~/Library/Scripts/rtlogin.sh $2"
     result=$?
