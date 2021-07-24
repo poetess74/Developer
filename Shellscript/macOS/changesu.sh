620e6e2ce37f54afbc83bcfae46cb6134d4e093b
HyeongminKim <assa0620@gmail.com>
2021-07-24 17:54:50 +0900

    modified:   changesu.sh

--- Shellscript/macOS/changesu.sh
+++ Shellscript/macOS/changesu.sh
@@ -7,13 +7,16 @@ if [ "$1" == "mysql.server" ]; then
     su bard -c "mysql.server $2"
     result=$?
 elif [ "$1" == "brewall" ]; then
-    su bard -c "~/Library/Scripts/brewall/brewall.sh;"
+    su bard -c "~/Library/Scripts/brewall/brewall.sh $2 $3;"
     ln -sF $(find /usr/local/Cellar/macvim/ -name "MacVim.app") ~/Applications/  
     result=$?
 
-    echo -e "\nEnter password for poetess. "
-    su poetess -c "source ~/.zshrc && youtube-dl -U && omz update"
-    result=$?
+    if [ $# == 1 ]; then
+        echo -e "\nEnter password for poetess. "
+        su poetess -c "source ~/.zshrc && youtube-dl -U && omz update"
+        result=$?
+    fi
+
 elif [ "$1" == "rtlogin" ]; then
     su bard -c "~/Library/Scripts/rtlogin.sh $2"
     result=$?
