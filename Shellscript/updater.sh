890f3038c53f3523117bca2163e1beefb582e5b3
HyeongminKim <assa0620@gmail.com>
2021-09-02 12:45:56 +0900

    modified:   Shellscript/updater.sh

--- Shellscript/updater.sh
+++ Shellscript/updater.sh
@@ -33,11 +33,6 @@ function gitUpdate() {
         git pull --rebase --stat origin $(git branch | sed '/* /!d'| sed 's/* //g')
         cd - > /dev/null
     fi
-    cd ~/Documents/Release/updater 2> /dev/null
-    if [ $? == 0 ]; then
-        git pull --rebase --stat origin $(git branch | sed '/* /!d'| sed 's/* //g')
-        cd - > /dev/null
-    fi
 }
 
 if [ "$SKIP_PACKAGE_UPDATER" == "true" ]; then
