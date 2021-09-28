47c6499824734abfe26d9e663b6796afad6e85c8
HyeongminKim <assa0620@gmail.com>
2021-09-28 18:19:31 +0900

    modified:   Shellscript/Windows/PowerShell/HideHidden.ps1

--- Shellscript/Windows/PowerShell/HideHidden.ps1
+++ Shellscript/Windows/PowerShell/HideHidden.ps1
@@ -1,4 +1,5 @@
 Remove-Item "C:\Users\HM\AppData\Roaming\Code\User\globalStorage\vscodevim.vim\.cmdline_history"
+Remove-Item "C:\Users\HM\AppData\Roaming\Code\User\globalStorage\vscodevim.vim\.search_history"
 
 Get-ChildItem "C:\Users\HM" -recurse -force | Where-Object {$_.name -like ".*" -and $_.attributes -match 'Hidden' -eq $false} | Set-ItemProperty -name Attributes -value ([System.IO.FileAttributes]::Hidden)
 
