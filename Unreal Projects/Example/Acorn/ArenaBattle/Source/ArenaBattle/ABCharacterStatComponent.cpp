d3a992e4893df593a76034ea35a1d2b4e76c1b0f
HyeongminKim <assa0620@gmail.com>
2021-09-29 15:08:22 +0900

    modified:   Unreal Projects/Example/Acorn/ArenaBattle/Content/Book/Maps/Step2.umap 	modified:   Unreal Projects/Example/Acorn/ArenaBattle/Source/ArenaBattle/ABCharacterStatComponent.cpp 	modified:   Unreal Projects/Example/Acorn/ArenaBattle/Source/ArenaBattle/ABCharacterStatComponent.h

--- Unreal Projects/Example/Acorn/ArenaBattle/Source/ArenaBattle/ABCharacterStatComponent.cpp
+++ Unreal Projects/Example/Acorn/ArenaBattle/Source/ArenaBattle/ABCharacterStatComponent.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ABCharacterStatComponent.h"
+#include "ABGameInstance.h"
 
 
 // Sets default values for this component's properties
@@ -8,9 +9,11 @@ UABCharacterStatComponent::UABCharacterStatComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
+	bWantsInitializeComponent = true;
 
-	// ...
+	Level = 1;
+
 }
 
 
@@ -19,16 +22,28 @@ void UABCharacterStatComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
 }
 
-
-// Called every frame
-void UABCharacterStatComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UABCharacterStatComponent::InitializeComponent()
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	Super::InitializeComponent();
+	SetNewLevel(Level);
 
-	// ...
 }
 
+void UABCharacterStatComponent::SetNewLevel(int32 NewLevel)
+{
+	auto ABGameInstance = Cast<UABGameInstance>(UGameplayStatics::GetGameInstance(GetWorld()));
+
+	ABCHECK(ABGameInstance != nullptr);
+	CurrentStatData = ABGameInstance->GetABCharacterData(NewLevel);
+	if(CurrentStatData != nullptr)
+	{
+		Level = NewLevel;
+		CurrentHP = CurrentStatData->MaxHP;
+	}
+	else
+	{
+		ABLOG(Error, TEXT("Level (%d) data does not exist."), NewLevel);
+	}
+}
