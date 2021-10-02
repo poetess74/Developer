f6c86d0ede39c3257f687705804437f3fdbef193
HyeongminKim <assa0620@gmail.com>
2021-10-02 21:46:28 +0900

    새로운 맵 생성 기능 추가

--- Unreal Projects/Example/Acorn/ArenaBattle/Source/ArenaBattle/Public/ABSection.h
+++ Unreal Projects/Example/Acorn/ArenaBattle/Source/ArenaBattle/Public/ABSection.h
@@ -50,4 +50,10 @@ private:
 	UPROPERTY(EditAnywhere, Category=State, Meta = (AllowPrivateAccess = true))
 	bool bNoBattle;
 
+	UFUNCTION()
+	void OnTriggerBeginOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
+
+	UFUNCTION()
+	void OnGateTriggerBeginOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
+
 };
