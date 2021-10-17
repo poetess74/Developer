372c28a2f6904047b8d9f87efe18d367f673d5d7
HyeongminKim <assa0620@gmail.com>
2021-10-17 13:06:22 +0900

    macOS 가 아닌 타 OS 에서 Sparkle 프레임워크 조사 방지

--- Shellscript/macOS/Private/extension.sh
+++ Shellscript/macOS/Private/extension.sh
@@ -55,6 +55,61 @@ if [ $? != 0 ]; then
     errorCount=$((errorCount+1))
 fi
 
+echo -e "\n\033[0;1mCheck for updates manually for the apps below that use the Sparkle framework.\033[m"
+noSuchSparkle="\033[33mWRN\033[m: Sparkle framework not found in"
+
+if [ -d /Applications ]; then
+    echo "--- System Applications ---"
+    find /Applications -name Sparkle.framework -maxdepth 7 -mindepth 3 1> ~/Library/Logs/find.log 2> /dev/null
+    if [ "$(cat ~/Library/Logs/find.log)" == "" ]; then
+        echo -e "$noSuchSparkle \033[0;4m/Applications\033[m"
+    else
+        grep -ioE '.*\.app' ~/Library/Logs/find.log > ~/Library/Logs/find1.log 2>/dev/null
+        while read line; do
+            appVersion=$(mdls -raw -name kMDItemVersion $line)
+            echo "$line ($appVersion)" >> ~/Library/Logs/find2.log
+        done < ~/Library/Logs/find1.log
+
+        nl ~/Library/Logs/find2.log
+
+        rm ~/Library/Logs/find.log ~/Library/Logs/find1.log ~/Library/Logs/find2.log 2> /dev/null
+    fi
+fi
+if [ -d /Volumes/External\ HD/Applications ]; then
+    echo -e "\n--- External Applications ---"
+    find /Volumes/External\ HD/Applications -name Sparkle.framework  -maxdepth 7 -mindepth 3 1> ~/Library/Logs/find.log 2> /dev/null
+    if [ "$(cat ~/Library/Logs/find.log)" == "" ]; then
+        echo -e "$noSuchSparkle \033[0;4m/Volumes/External HD/Applications\033[m"
+    else
+        grep -ioE '.*\.app' ~/Library/Logs/find.log > ~/Library/Logs/find1.log 2>/dev/null
+        while read line; do
+            appVersion=$(mdls -raw -name kMDItemVersion "$line")
+            echo "$line ($appVersion)" >> ~/Library/Logs/find2.log
+        done < ~/Library/Logs/find1.log
+
+        nl ~/Library/Logs/find2.log
+
+        rm ~/Library/Logs/find.log ~/Library/Logs/find1.log ~/Library/Logs/find2.log 2> /dev/null
+    fi
+fi
+if [ -d $HOME/Applications ]; then
+    echo -e "\n--- User Applications ---"
+    find ~/Applications -name Sparkle.framework  -maxdepth 7 -mindepth 3 1> ~/Library/Logs/find.log 2> /dev/null
+    if [ "$(cat ~/Library/Logs/find.log)" == "" ]; then
+        echo -e "$noSuchSparkle \033[0;4m$HOME/Applications\033[m"
+    else
+        grep -ioE '.*\.app' ~/Library/Logs/find.log > ~/Library/Logs/find1.log 2>/dev/null
+        while read line; do
+            appVersion=$(mdls -raw -name kMDItemVersion $line)
+            echo "$line ($appVersion)" >> ~/Library/Logs/find2.log
+        done < ~/Library/Logs/find1.log
+
+        nl ~/Library/Logs/find2.log
+
+        rm ~/Library/Logs/find.log ~/Library/Logs/find1.log ~/Library/Logs/find2.log 2> /dev/null
+    fi
+fi
+
 /bin/zsh -c "source ~/.zshrc; omz update && exit"
 if [ $? != 0 ]; then
     errorCount=$((errorCount+1))
--- Shellscript/updater.sh
+++ Shellscript/updater.sh
@@ -35,65 +35,8 @@ function gitUpdate() {
     fi
 }
 
-function sparkleUpdate() {
-    echo -e "\n\033[0;1mCheck for updates manually for the apps below that use the Sparkle framework.\033[m"
-    noSuchSparkle="\033[33mWRN\033[m: Sparkle framework not found in"
-    if [ -d /Applications ]; then
-        echo "--- System Applications ---"
-        find /Applications -name Sparkle.framework -maxdepth 7 -mindepth 3 1> ~/Library/Logs/find.log 2> /dev/null
-        if [ "$(cat ~/Library/Logs/find.log)" == "" ]; then
-            echo -e "$noSuchSparkle \033[0;4m/Applications\033[m"
-        else
-            grep -ioE '.*\.app' ~/Library/Logs/find.log > ~/Library/Logs/find1.log 2>/dev/null
-            while read line; do
-                appVersion=$(mdls -raw -name kMDItemVersion $line)
-                echo "$line ($appVersion)" >> ~/Library/Logs/find2.log
-            done < ~/Library/Logs/find1.log
-
-            nl ~/Library/Logs/find2.log
-
-            rm ~/Library/Logs/find.log ~/Library/Logs/find1.log ~/Library/Logs/find2.log 2> /dev/null
-        fi
-    fi
-    if [ -d /Volumes/External\ HD/Applications ]; then
-        echo -e "\n--- External Applications ---"
-        find /Volumes/External\ HD/Applications -name Sparkle.framework  -maxdepth 7 -mindepth 3 1> ~/Library/Logs/find.log 2> /dev/null
-        if [ "$(cat ~/Library/Logs/find.log)" == "" ]; then
-            echo -e "$noSuchSparkle \033[0;4m/Volumes/External HD/Applications\033[m"
-        else
-            grep -ioE '.*\.app' ~/Library/Logs/find.log > ~/Library/Logs/find1.log 2>/dev/null
-            while read line; do
-                appVersion=$(mdls -raw -name kMDItemVersion "$line")
-                echo "$line ($appVersion)" >> ~/Library/Logs/find2.log
-            done < ~/Library/Logs/find1.log
-
-            nl ~/Library/Logs/find2.log
-
-            rm ~/Library/Logs/find.log ~/Library/Logs/find1.log ~/Library/Logs/find2.log 2> /dev/null
-        fi
-    fi
-    if [ -d $HOME/Applications ]; then
-        echo -e "\n--- User Applications ---"
-        find ~/Applications -name Sparkle.framework  -maxdepth 7 -mindepth 3 1> ~/Library/Logs/find.log 2> /dev/null
-        if [ "$(cat ~/Library/Logs/find.log)" == "" ]; then
-            echo -e "$noSuchSparkle \033[0;4m$HOME/Applications\033[m"
-        else
-            grep -ioE '.*\.app' ~/Library/Logs/find.log > ~/Library/Logs/find1.log 2>/dev/null
-            while read line; do
-                appVersion=$(mdls -raw -name kMDItemVersion $line)
-                echo "$line ($appVersion)" >> ~/Library/Logs/find2.log
-            done < ~/Library/Logs/find1.log
-
-            nl ~/Library/Logs/find2.log
-
-            rm ~/Library/Logs/find.log ~/Library/Logs/find1.log ~/Library/Logs/find2.log 2> /dev/null
-        fi
-    fi
-}
-
 if [ "$SKIP_PACKAGE_UPDATER" == "true" ]; then
     gitUpdate
-    sparkleUpdate
     echo -e '\a'
     exit $exitCode
 fi
@@ -105,7 +48,6 @@ if [ "$(uname -s)" == "Darwin" ]; then
     else
         gitUpdate
     fi
-    sparkleUpdate
 elif [ "$(uname -s)" == "Linux" ]; then
     if [ -x ~/Documents/Release/aptall/aptall.sh ]; then
         ~/Documents/Release/aptall/aptall.sh $@
@@ -113,10 +55,8 @@ elif [ "$(uname -s)" == "Linux" ]; then
     else
         gitUpdate
     fi
-    sparkleUpdate
 else
     gitUpdate
-    sparkleUpdate
 fi
 
 echo -e '\a'
