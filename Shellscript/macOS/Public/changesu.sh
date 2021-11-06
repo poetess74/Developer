ddb365815c4910973ae4dcc47c9fe2a80e977009
HyeongminKim <assa0620@gmail.com>
2021-11-06 12:54:00 +0900

    modified:   Shellscript/macOS/Public/changesu.sh

--- Shellscript/macOS/Public/changesu.sh
+++ Shellscript/macOS/Public/changesu.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 cd ~
-echo "Enter password for bard. "
 
 if [ "$1" == "mysql.server" ]; then
-    su bard -c "mysql.server $2"
+    which mysql > /dev/null
+    if [ $? == 0 ]; then
+        echo "Enter password for bard. "
+        su bard -c "mysql.server $2"
+    else
+        echo "mysql not found"
+    fi
     result=$?
 elif [ "$1" == "brewall" ]; then
+    echo "Enter password for bard. "
     su bard -c "~/Library/Scripts/brewall/brewall.sh $2 $3; echo ''"
     if [ $? == 0 ]; then
         ln -sF $(find /usr/local/Cellar/macvim/ -name "MacVim.app") ~/Applications/  
@@ -19,6 +25,7 @@ elif [ "$1" == "brewall" ]; then
         result=$?
     fi
 elif [ "$1" == "rtlogin" ]; then
+    echo "Enter password for bard. "
     su bard -c "~/Library/Scripts/rtlogin.sh $2"
     result=$?
 fi
